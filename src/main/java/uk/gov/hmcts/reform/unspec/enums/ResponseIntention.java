8581b75960a480941e63886da50e0636baebe731
Piotr Dudkiewicz <58216515+pd-dudkiewicz@users.noreply.github.com>
2021-01-27 13:56:44 +0000

    feat: removed service and ccd files (CMC-1162)

--- src/main/java/uk/gov/hmcts/reform/unspec/enums/ResponseIntention.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.hmcts.reform.unspec.enums;
-
-public enum ResponseIntention {
-    FULL_DEFENCE,
-    PART_DEFENCE,
-    CONTEST_JURISDICTION
-}
--- src/main/java/uk/gov/hmcts/reform/unspec/model/documents/DocumentType.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.reform.unspec.model.documents;
-
-public enum DocumentType {
-    SEALED_CLAIM,
-    CERTIFICATE_OF_SERVICE,
-    ACKNOWLEDGEMENT_OF_SERVICE,
-    DIRECTIONS_QUESTIONNAIRE
-}
