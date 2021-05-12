24c3eec5b1b4ee6827f5b7fb6ce493eb824294fc
Sam Walker <sam.walker@hmcts.net>
2021-05-12 11:55:36 +0100

    fix: update cycle times of schedulers (#37)

--- src/test/java/uk/gov/hmcts/reform/unspec/bpmn/PollingEventEmitterSchedulerTest.java
+++ src/test/java/uk/gov/hmcts/reform/unspec/bpmn/PollingEventEmitterSchedulerTest.java
@@ -2,9 +2,12 @@ package uk.gov.hmcts.reform.unspec.bpmn;
 
 import org.camunda.bpm.engine.externaltask.ExternalTask;
 import org.camunda.bpm.engine.externaltask.LockedExternalTask;
+import org.camunda.bpm.engine.impl.calendar.CronExpression;
 import org.camunda.bpm.engine.management.JobDefinition;
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,7 +22,7 @@ class PollingEventEmitterSchedulerTest extends BpmnBaseTest {
     }
 
     @Test
-    void pollingEventBmpnShouldFirePollingEventEmmiterExternalTask_whenStarted() {
+    void pollingEventBmpnShouldFirePollingEventEmmiterExternalTask_whenStarted() throws ParseException {
         //assert process has started
         assertFalse(processInstance.isEnded());
 
@@ -32,7 +35,14 @@ class PollingEventEmitterSchedulerTest extends BpmnBaseTest {
         //assert that job is as expected
         assertThat(jobDefinitions).hasSize(1);
         assertThat(jobDefinitions.get(0).getJobType()).isEqualTo("timer-start-event");
-        assertThat(jobDefinitions.get(0).getJobConfiguration()).isEqualTo("CYCLE: 0 0/30 * * * ?");
+
+        String cronString = "0 0/5 * * * ?";
+        assertThat(jobDefinitions.get(0).getJobConfiguration()).isEqualTo("CYCLE: " + cronString);
+        assertCronTriggerFiresAtExpectedTime(
+            new CronExpression(cronString),
+            LocalDateTime.of(2020, 1, 1, 0, 0, 0),
+            LocalDateTime.of(2020, 1, 1, 0, 5, 0)
+        );
 
         //get external tasks
         List<ExternalTask> externalTasks = getExternalTasks();
