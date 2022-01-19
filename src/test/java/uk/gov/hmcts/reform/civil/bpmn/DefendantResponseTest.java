c57cf41276c50c2483e0e0c09e51943cab7372a8
Harry H <harry.henry@version1.com>
2022-01-19 11:36:44 +0000

    Refactoring Activity IDs for Generate DQ + Covering all FlowState Test scenarios

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/DefendantResponseTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/DefendantResponseTest.java
@@ -39,11 +39,12 @@ class DefendantResponseTest extends BpmnBaseTest {
     private static final String FULL_DEFENCE_RESPONSE_ACTIVITY_ID = "FullDefenceResponse";
     private static final String FULL_DEFENCE_NOTIFICATION_ACTIVITY_ID
         = "DefendantResponseFullDefenceNotifyApplicantSolicitor1";
-    //to be removed when new DQ is created for respondent solicitor 2
-    public static final String ONE_RESPONSE_RECEIVED_TEMP_EMAIL_ACTIVITY_ID
-        = "DefendantResponseOneResponseReceivedNotifyApplicantSolicitor1";
     private static final String FULL_DEFENCE_GENERATE_DIRECTIONS_QUESTIONNAIRE_ACTIVITY_ID
         = "DefendantResponseFullDefenceGenerateDirectionsQuestionnaire";
+    private static final String FIRST_RESPONSE_FULL_DEFENCE_GENERATE_DIRECTIONS_QUESTIONNAIRE_ACTIVITY_ID
+        = "FirstResponseFullDefenceGenerateDirectionsQuestionnaire";
+    private static final String DIVERGENT_WITH_FULL_DEFENCE_GENERATE_DIRECTIONS_QUESTIONNAIRE_ACTIVITY_ID
+        = "DivergentDefendantResponseWithFullDefenceGenerateDirectionsQuestionnaire";
     private static final String NOTIFY_RPA_ON_CASE_HANDED_OFFLINE_ACTIVITY_ID = "NotifyRoboticsOnCaseHandedOffline";
     private static final String NOTIFY_RPA_ON_CONTINUOUS_FEED_ACTIVITY_ID = "NotifyRoboticsOnContinuousFeed";
 
@@ -118,7 +119,7 @@ class DefendantResponseTest extends BpmnBaseTest {
     }
 
     @Test
-    void shouldSuccessfullyCompleteOfflineDefendantResponse_In1v2_DivergentResponseScenario() {
+    void shouldSuccessfullyGoOfflineDefendantResponse_In1v2_DivergentResponseScenario() {
         //assert process has started
         assertFalse(processInstance.isEnded());
 
@@ -127,7 +128,7 @@ class DefendantResponseTest extends BpmnBaseTest {
 
         //Setup Case as 1v2 All Responses Received > Divergent Response
         VariableMap variables = Variables.createVariables();
-        variables.putValue("flowState", "MAIN.DIVERGENT_RESPOND");
+        variables.putValue("flowState", "MAIN.DIVERGENT_RESPOND_GO_OFFLINE");
         variables.put(FLOW_FLAGS, Map.of(ONE_RESPONDENT_REPRESENTATIVE, false));
         variables.put(FLOW_FLAGS, Map.of(TWO_RESPONDENT_REPRESENTATIVES, true));
 
@@ -138,6 +139,84 @@ class DefendantResponseTest extends BpmnBaseTest {
             START_BUSINESS_TOPIC,
             START_BUSINESS_EVENT,
             START_BUSINESS_ACTIVITY,
+            variables
+        );
+
+        //complete the proceedOffline event
+        ExternalTask proceedOffline = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(
+            proceedOffline,
+            PROCESS_CASE_EVENT,
+            TAKE_CASE_OFFLINE_EVENT,
+            TAKE_CASE_OFFLINE_ACTIVITY_ID,
+            variables
+        );
+
+        //complete the notification to respondent
+        ExternalTask notifyRespondent = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(
+            notifyRespondent,
+            PROCESS_CASE_EVENT,
+            OFFLINE_NOTIFY_RESPONDENT_SOLICITOR_1,
+            OFFLINE_NOTIFICATION_RESPONDENT_ACTIVITY_ID
+        );
+
+        //complete the notification to applicant
+        ExternalTask forApplicant = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(
+            forApplicant,
+            PROCESS_CASE_EVENT,
+            OFFLINE_NOTIFY_APPLICANT_SOLICITOR_1,
+            OFFLINE_NOTIFICATION_APPLICANT_ACTIVITY_ID
+        );
+
+        //complete the Robotics notification
+        ExternalTask forRobotics = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(
+            forRobotics,
+            PROCESS_CASE_EVENT,
+            NOTIFY_RPA_ON_CASE_HANDED_OFFLINE,
+            NOTIFY_RPA_ON_CASE_HANDED_OFFLINE_ACTIVITY_ID
+        );
+
+        //end business process
+        ExternalTask endBusinessProcess = assertNextExternalTask(END_BUSINESS_PROCESS);
+        completeBusinessProcess(endBusinessProcess);
+
+        assertNoExternalTasksLeft();
+    }
+
+    @Test
+    void shouldSuccessfullyGenerateDQAndGoOfflineDefendantResponse_In1v2_DivergentResponseScenarioWithFullDefence() {
+        //assert process has started
+        assertFalse(processInstance.isEnded());
+
+        //assert message start event
+        assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
+
+        //Setup Case as 1v2 All Responses Received > Divergent Response
+        VariableMap variables = Variables.createVariables();
+        variables.putValue("flowState", "MAIN.DIVERGENT_RESPOND_GENERATE_DQ_GO_OFFLINE");
+        variables.put(FLOW_FLAGS, Map.of(ONE_RESPONDENT_REPRESENTATIVE, false));
+        variables.put(FLOW_FLAGS, Map.of(TWO_RESPONDENT_REPRESENTATIVES, true));
+
+        //complete the start business process
+        ExternalTask startBusinessTask = assertNextExternalTask(START_BUSINESS_TOPIC);
+        assertCompleteExternalTask(
+            startBusinessTask,
+            START_BUSINESS_TOPIC,
+            START_BUSINESS_EVENT,
+            START_BUSINESS_ACTIVITY,
+            variables
+        );
+
+        //complete the document generation
+        ExternalTask documentGeneration = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(
+            documentGeneration,
+            PROCESS_CASE_EVENT,
+            FULL_DEFENCE_GENERATE_DIRECTIONS_QUESTIONNAIRE,
+            DIVERGENT_WITH_FULL_DEFENCE_GENERATE_DIRECTIONS_QUESTIONNAIRE_ACTIVITY_ID,
             variables
         );
 
@@ -269,7 +348,7 @@ class DefendantResponseTest extends BpmnBaseTest {
     }
 
     @Test
-    void shouldSuccessfullyCompleteOneTask_whenOnlyOneResponseReceivedIn1v2Case() {
+    void shouldSuccessfullyCompleteOneTask_whenOnlyOneResponseReceivedWithFullDefenceIn1v2Case() {
         //assert process has started
         assertFalse(processInstance.isEnded());
 
@@ -280,7 +359,7 @@ class DefendantResponseTest extends BpmnBaseTest {
         ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
 
         VariableMap variables = Variables.createVariables();
-        variables.putValue("flowState", "MAIN.AWAITING_RESPONSES_RECEIVED");
+        variables.putValue("flowState", "MAIN.AWAITING_RESPONSES_FULL_DEFENCE_RECEIVED");
 
         assertCompleteExternalTask(
             startBusiness,
@@ -290,29 +369,44 @@ class DefendantResponseTest extends BpmnBaseTest {
             variables
         );
 
-        // TODO: this will be refactored to be a DQ rather than an applicant notification. Covered in CMC-1653
-        // notifyApplicant is a placeholder until we can generate a DQ for respondent 2 solicitor
-        ExternalTask notifyApplicant = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            notifyApplicant,
-            PROCESS_CASE_EVENT,
-            FULL_DEFENCE_NOTIFY_APPLICANT_SOLICITOR_1,
-            ONE_RESPONSE_RECEIVED_TEMP_EMAIL_ACTIVITY_ID,
-            variables
-        );
-
-        //TODO: Remove the above and uncomment this when CMC-1653 is completed
-        //complete the DQ document generation
-        /*
+        //complete the document generation
         ExternalTask documentGeneration = assertNextExternalTask(PROCESS_CASE_EVENT);
         assertCompleteExternalTask(
             documentGeneration,
             PROCESS_CASE_EVENT,
             FULL_DEFENCE_GENERATE_DIRECTIONS_QUESTIONNAIRE,
-            ONE_RESPONSE_RECEIVED_GENERATE_DIRECTIONS_QUESTIONNAIRE_ACTIVITY_ID,
+            FIRST_RESPONSE_FULL_DEFENCE_GENERATE_DIRECTIONS_QUESTIONNAIRE_ACTIVITY_ID,
             variables
         );
-         */
+
+        //end business process
+        ExternalTask endBusinessProcess = assertNextExternalTask(END_BUSINESS_PROCESS);
+        completeBusinessProcess(endBusinessProcess);
+
+        assertNoExternalTasksLeft();
+    }
+
+    @Test
+    void shouldSuccessfullyCompleteOneTask_whenOnlyOneResponseReceivedWithoutFullDefenceIn1v2Case() {
+        //assert process has started
+        assertFalse(processInstance.isEnded());
+
+        //assert message start event
+        assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
+
+        //complete the start business process
+        ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
+
+        VariableMap variables = Variables.createVariables();
+        variables.putValue("flowState", "MAIN.AWAITING_RESPONSES_NOT_FULL_DEFENCE_RECEIVED");
+
+        assertCompleteExternalTask(
+            startBusiness,
+            START_BUSINESS_TOPIC,
+            START_BUSINESS_EVENT,
+            START_BUSINESS_ACTIVITY,
+            variables
+        );
 
         //end business process
         ExternalTask endBusinessProcess = assertNextExternalTask(END_BUSINESS_PROCESS);
