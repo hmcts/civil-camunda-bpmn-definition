85c9ad6a8ddfd4b6718d07d373fcab8eb5b7ac7c
GarethLancaster <31533575+Gareth40342@users.noreply.github.com>
2022-02-09 13:00:48 +0000

    Fixed checkstyle issues

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/AddDefendantLitigationFriendTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/AddDefendantLitigationFriendTest.java
@@ -6,7 +6,6 @@ import org.camunda.bpm.engine.variable.Variables;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Map;
 
@@ -69,7 +68,7 @@ class AddDefendantLitigationFriendTest extends BpmnBaseTest {
             variables
         );
 
-        if(twoRespondents) {
+        if (twoRespondents) {
             //complete the CC notification to respondent solicitor 2
             notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
             assertCompleteExternalTask(
