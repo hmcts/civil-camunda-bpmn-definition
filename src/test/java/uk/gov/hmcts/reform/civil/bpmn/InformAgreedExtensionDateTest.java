e800a9ca012c46857fe8b28b4650a00820809985
Doug Rice <douglasrice@yahoo.com>
2022-03-18 10:30:29 +0000

    removed unused import statement

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/InformAgreedExtensionDateTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/InformAgreedExtensionDateTest.java
@@ -6,7 +6,6 @@ import org.camunda.bpm.engine.variable.Variables;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Map;
 
