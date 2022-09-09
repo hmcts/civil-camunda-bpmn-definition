92ad3054e90b9fb943e813e424c9a621babe3e26
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-09-09 12:44:05 +0100

    CIV-4571: Update for check style

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/DefendantContactDetailsChangeCuiTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/DefendantContactDetailsChangeCuiTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.civil.bpmn;
 import org.camunda.bpm.engine.externaltask.ExternalTask;
 import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.Variables;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
