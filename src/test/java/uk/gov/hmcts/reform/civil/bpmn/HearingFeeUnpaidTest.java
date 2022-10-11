5d4f453387a6ac1b6d1345e0e0e9929888c382ea
meulendijksm <max.meulendijks@hmcts.net>
2022-10-11 09:35:43 +0100

    added test

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/HearingFeeUnpaidTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/HearingFeeUnpaidTest.java
@@ -17,25 +17,14 @@ class HearingFeeUnpaidTest extends BpmnBaseTest {
     public static final String MESSAGE_NAME = "HEARING_FEE_UNPAID";
     public static final String PROCESS_ID = "HEARING_FEE_UNPAID";
 
-<<<<<<< Updated upstream
     public HearingFeeUnpaidTest() {
         super("hearing_fee_unpaid.bpmn", "HEARING_FEE_UNPAID");
-=======
-    private static final String NOTIFY_RPA_ON_CASE_HANDED_OFFLINE = "NOTIFY_RPA_ON_CASE_HANDED_OFFLINE";
-    private static final String NOTIFY_RPA_ON_CASE_HANDED_OFFLINE_ACTIVITY_ID = "NotifyRoboticsOnCaseHandedOffline";
-
-    public HearingFeeUnpaidTest() {
-        super("take_case_offline.bpmn", "TAKE_CASE_OFFLINE");
->>>>>>> Stashed changes
     }
 
     @ParameterizedTest
     @CsvSource({"true", "false"})
-<<<<<<< Updated upstream
     void shouldSuccessfullyCompleteHearingFeeUnpaidMultiparty(boolean twoRepresentatives) {
-=======
-    void shouldSuccessfullyCompleteTakeCaseOfflineMultiparty(boolean twoRepresentatives) {
->>>>>>> Stashed changes
+
         //assert process has started
         assertFalse(processInstance.isEnded());
 
@@ -58,7 +47,6 @@ class HearingFeeUnpaidTest extends BpmnBaseTest {
             variables
         );
 
-<<<<<<< Updated upstream
         //complete the notification for respondent 1
         ExternalTask respondentNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
         assertCompleteExternalTask(respondentNotification,
@@ -83,110 +71,6 @@ class HearingFeeUnpaidTest extends BpmnBaseTest {
                                    PROCESS_CASE_EVENT,
                                    "NOTIFY_APPLICANT_SOLICITOR1_FOR_HEARING_FEE_UNPAID",
                                    "HearingFeeUnpaidNotifyApplicantSolicitor1"
-=======
-        //complete the RPA notification
-        ExternalTask rpaNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            rpaNotification,
-            PROCESS_CASE_EVENT,
-            NOTIFY_RPA_ON_CASE_HANDED_OFFLINE,
-            NOTIFY_RPA_ON_CASE_HANDED_OFFLINE_ACTIVITY_ID
-        );
-
-        ExternalTask respondentNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(respondentNotification,
-                                   PROCESS_CASE_EVENT,
-                                   "NOTIFY_RESPONDENT_SOLICITOR1_FOR_CASE_TAKEN_OFFLINE",
-                                   "TakeCaseOfflineNotifyRespondentSolicitor1"
-        );
-
-        if (twoRepresentatives) {
-            //complete the notification to respondent 2
-            ExternalTask respondent2Notification = assertNextExternalTask(PROCESS_CASE_EVENT);
-            assertCompleteExternalTask(respondent2Notification,
-                                       PROCESS_CASE_EVENT,
-                                       "NOTIFY_RESPONDENT_SOLICITOR2_FOR_CASE_TAKEN_OFFLINE",
-                                       "TakeCaseOfflineNotifyRespondentSolicitor2"
-            );
-        }
-
-        //complete the notification to applicant
-        ExternalTask applicantNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(applicantNotification,
-                                   PROCESS_CASE_EVENT,
-                                   "NOTIFY_APPLICANT_SOLICITOR1_FOR_CASE_TAKEN_OFFLINE",
-                                   "TakeCaseOfflineNotifyApplicantSolicitor1"
-        );
-
-        //end business process
-        ExternalTask endBusinessProcess = assertNextExternalTask(END_BUSINESS_PROCESS);
-        completeBusinessProcess(endBusinessProcess);
-
-        assertNoExternalTasksLeft();
-    }
-
-    @ParameterizedTest
-    @CsvSource({"true, true", "true, false", "false, true", "true, null"})
-    void shouldSuccessfullyCompleteTakeCaseOfflineUnrepresentedDefendant(boolean unrepresentedDefendant1,
-                                                                         boolean unrepresentedDefendant2) {
-        //assert process has started
-        assertFalse(processInstance.isEnded());
-
-        //assert message start event
-        assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
-
-        VariableMap variables = Variables.createVariables();
-        variables.put("flowFlags", Map.of(
-            UNREPRESENTED_DEFENDANT_ONE, unrepresentedDefendant1,
-            UNREPRESENTED_DEFENDANT_TWO, unrepresentedDefendant2
-        ));
-
-        //complete the start business process
-        ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
-        assertCompleteExternalTask(
-            startBusiness,
-            START_BUSINESS_TOPIC,
-            START_BUSINESS_EVENT,
-            START_BUSINESS_ACTIVITY,
-            variables
-        );
-
-        //complete the RPA notification
-        ExternalTask rpaNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            rpaNotification,
-            PROCESS_CASE_EVENT,
-            NOTIFY_RPA_ON_CASE_HANDED_OFFLINE,
-            NOTIFY_RPA_ON_CASE_HANDED_OFFLINE_ACTIVITY_ID
-        );
-
-        //complete the notification to respondent 1
-        if (!unrepresentedDefendant1) {
-            ExternalTask respondentNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
-            assertCompleteExternalTask(respondentNotification,
-                                       PROCESS_CASE_EVENT,
-                                       "NOTIFY_RESPONDENT_SOLICITOR1_FOR_CASE_TAKEN_OFFLINE",
-                                       "TakeCaseOfflineNotifyRespondentSolicitor1"
-            );
-        }
-
-        if (!unrepresentedDefendant2) {
-            //complete the notification to respondent 2
-            ExternalTask respondent2Notification = assertNextExternalTask(PROCESS_CASE_EVENT);
-            assertCompleteExternalTask(respondent2Notification,
-                                       PROCESS_CASE_EVENT,
-                                       "NOTIFY_RESPONDENT_SOLICITOR2_FOR_CASE_TAKEN_OFFLINE",
-                                       "TakeCaseOfflineNotifyRespondentSolicitor2"
-            );
-        }
-
-        //complete the notification to applicant
-        ExternalTask applicantNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(applicantNotification,
-                                   PROCESS_CASE_EVENT,
-                                   "NOTIFY_APPLICANT_SOLICITOR1_FOR_CASE_TAKEN_OFFLINE",
-                                   "TakeCaseOfflineNotifyApplicantSolicitor1"
->>>>>>> Stashed changes
         );
 
         //end business process
