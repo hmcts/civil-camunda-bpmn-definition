5eb4a12583aaf041678746a804959deb349aeca4
Bhagyashree Sharma <bhagyashree.sharma@version1.com>
2022-11-01 22:39:19 +0000

    Added camnuda scheduler for evidence upload and camunda process to send notification

---
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/EvidenceUploadNotifyTest.java
@@ -0,0 +1,76 @@
+package uk.gov.hmcts.reform.civil.bpmn;
+
+import org.camunda.bpm.engine.externaltask.ExternalTask;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class EvidenceUploadNotifyTest extends BpmnBaseTest {
+
+    public static final String MESSAGE_NAME = "EVIDENCE_UPLOAD_NOTIFY";
+    public static final String PROCESS_ID = "EVIDENCE_UPLOAD_NOTIFY";
+
+    public EvidenceUploadNotifyTest() {
+        super("evidence_upload_notify.bpmn", "EVIDENCE_UPLOAD_NOTIFY");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"true", "false"})
+    void shouldSuccessfullyCompleteEvidenceUploadNotify(boolean twoRepresentatives) {
+
+        //assert process has started
+        assertFalse(processInstance.isEnded());
+
+        //assert message start event
+        assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
+
+        //complete the start business process
+        ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
+        assertCompleteExternalTask(
+            startBusiness,
+            START_BUSINESS_TOPIC,
+            START_BUSINESS_EVENT,
+            START_BUSINESS_ACTIVITY
+        );
+
+        //complete the notification for respondent 1
+        ExternalTask respondentNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(respondentNotification,
+                                   PROCESS_CASE_EVENT,
+                                   "NOTIFY_RESPONDENT_SOLICITOR_FOR_EVIDENCE_UPLOAD",
+                                   "EvidenceUploadNotifyRespondentSolicitors"
+        );
+
+        //complete the notification for applicant solicitor
+        ExternalTask applicantNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(applicantNotification,
+                                   PROCESS_CASE_EVENT,
+                                   "NOTIFY_APPLICANT_SOLICITOR_FOR_EVIDENCE_UPLOAD",
+                                   "EvidenceUploadNotifyApplicantSolicitor"
+        );
+
+        //end business process
+        ExternalTask endBusinessProcess = assertNextExternalTask(END_BUSINESS_PROCESS);
+        completeBusinessProcess(endBusinessProcess);
+
+        assertNoExternalTasksLeft();
+    }
+
+    @Test
+    void shouldAbort_whenStartBusinessProcessThrowsAnError() {
+        //assert process has started
+        assertFalse(processInstance.isEnded());
+
+        //assert message start event
+        assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
+
+        //fail the start business process
+        ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
+        assertFailExternalTask(startBusiness, START_BUSINESS_TOPIC, START_BUSINESS_EVENT, START_BUSINESS_ACTIVITY);
+
+        assertNoExternalTasksLeft();
+    }
+}
