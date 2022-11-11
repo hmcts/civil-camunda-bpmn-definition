a79f61fd861913ebe816ced35bdb4a42a33d8734
Nagaraju Deekonda <nagaraju.deekonda@version1.com>
2022-11-11 18:45:23 +0000

    updated with latest changes

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/CreateClaimSpecAfterPaymentTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/CreateClaimSpecAfterPaymentTest.java
@@ -11,24 +11,13 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-public class CreateClaimSpecTest extends BpmnBaseTest {
+public class CreateClaimSpecAfterPaymentTest extends BpmnBaseTest {
 
     //BPMN Settings
     private static final String MESSAGE_NAME = "CREATE_CLAIM_SPEC";
     private static final String PROCESS_ID = "CREATE_CLAIM_PROCESS_ID_SPEC";
     private static final String FLOW_STATE = "flowState";
     private static final String FLOW_FLAGS = "flowFlags";
-    //assign case access
-    private static final String CASE_ASSIGNMENT_EVENT = "ASSIGN_CASE_TO_APPLICANT_SOLICITOR1_SPEC";
-    private static final String CASE_ASSIGNMENT_ACTIVITY_ID = "CaseAssignmentToApplicantSolicitor1ForSpec";
-    //validate fee
-    private static final String VALIDATE_FEE_EVENT = "VALIDATE_FEE_SPEC";
-    private static final String VALIDATE_FEE_ACTIVITY_ID = "ValidateClaimFeeForSpec";
-    //payment
-    public static final String MAKE_PBA_PAYMENT_EVENT = "MAKE_PBA_PAYMENT_SPEC";
-    private static final String MAKE_PAYMENT_ACTIVITY_ID = "CreateClaimMakePaymentForSpec";
-    private static final String PROCESS_PAYMENT_TOPIC = "processPayment";
-    //generate claim form
     private static final String GENERATE_CLAIM_FORM_EVENT = "GENERATE_CLAIM_FORM_SPEC";
     private static final String GENERATE_CLAIM_FORM_ACTIVITY_ID = "GenerateClaimFormForSpec";
     //issue claim
@@ -65,11 +54,6 @@ public class CreateClaimSpecTest extends BpmnBaseTest {
         = "NOTIFY_APPLICANT_SOLICITOR1_FOR_CASE_TAKEN_OFFLINE_SPEC";
     private static final String NOTIFY_APPLICANT_SOLICITOR1_FOR_CASE_TAKEN_OFFLINE_SPEC_ACTIVITY_ID
         = "TakeCaseOfflineForSpecNotifyApplicantSolicitor1";
-    //notify applicant solicitor 1 payment fail
-    private static final String NOTIFY_APPLICANT_SOLICITOR1_FOR_FAILED_PAYMENT_SPEC_EVENT
-        = "NOTIFY_APPLICANT_SOLICITOR1_FOR_FAILED_PAYMENT_SPEC";
-    private static final String NOTIFY_APPLICANT_SOLICITOR1_FOR_FAILED_PAYMENT_SPEC_ACTIVITY_ID
-        = "CreateClaimPaymentFailedForSpecNotifyApplicantSolicitor1";
     //notify respondent solicitor 1
     private static final String NOTIFY_RESPONDENT_SOLICITOR1_FOR_CLAIM_CONTINUING_ONLINE_SPEC_EVENT
         = "NOTIFY_RESPONDENT_SOLICITOR1_FOR_CLAIM_CONTINUING_ONLINE_SPEC";
@@ -85,17 +69,13 @@ public class CreateClaimSpecTest extends BpmnBaseTest {
         = "NOTIFY_RESPONDENT1_FOR_CLAIM_CONTINUING_ONLINE_SPEC";
     private static final String NOTIFY_RESPONDENT1_FOR_CLAIM_CONTINUING_ONLINE_SPEC_ACTIVITY_ID
         = "CreateClaimContinuingOnlineNotifyRespondent1ForSpec";
-    //payment failed
-    private static final String PROCESS_PAYMENT_FAILED_EVENT = "PROCESS_PAYMENT_FAILED";
-    private static final String PROCESS_PAYMENT_FAILED_ACTIVITY_ID = "PaymentFailed";
+
+    public CreateClaimSpecAfterPaymentTest() {
+        super("create_claim_spec_after_payment.bpmn", "CREATE_CLAIM_PROCESS_ID_SPEC");
+    }
 
     enum FlowState {
-        CLAIM_ISSUED_PAYMENT_FAILED,
-        PAYMENT_FAILED,
-        CLAIM_ISSUED_PAYMENT_SUCCESSFUL,
-        PAYMENT_SUCCESSFUL,
         PENDING_CLAIM_ISSUED,
-        AWAITING_CASE_NOTIFICATION,
         PENDING_CLAIM_ISSUED_UNREPRESENTED_DEFENDANT,
         PENDING_CLAIM_ISSUED_UNREPRESENTED_DEFENDANT_ONE_V_ONE_SPEC,
         PENDING_CLAIM_ISSUED_UNREGISTERED_DEFENDANT;
@@ -103,10 +83,6 @@ public class CreateClaimSpecTest extends BpmnBaseTest {
         public String fullName() {
             return "MAIN" + "." + name();
         }
-    }
-
-    public CreateClaimSpecTest() {
-        super("create_claim_spec.bpmn", "CREATE_CLAIM_PROCESS_ID_SPEC");
     }
 
     @Nested
@@ -126,16 +102,6 @@ public class CreateClaimSpecTest extends BpmnBaseTest {
                 "PIP_ENABLED", true));
 
             startBusinessProcess(variables);
-
-            //complete the case assignment process
-            completeCaseAssignment(variables);
-
-            //validate the fee
-            validateFee(variables);
-
-            //complete the payment
-            variables.putValue(FLOW_STATE, FlowState.CLAIM_ISSUED_PAYMENT_SUCCESSFUL.fullName());
-            completePayment(variables);
 
             //complete the document generation
             variables.putValue(
@@ -203,16 +169,6 @@ public class CreateClaimSpecTest extends BpmnBaseTest {
             //complete the start business process
             startBusinessProcess(variables);
 
-            //complete the case assignment process
-            completeCaseAssignment(variables);
-
-            //validate the fee
-            validateFee(variables);
-
-            //complete the payment
-            variables.putValue(FLOW_STATE, FlowState.CLAIM_ISSUED_PAYMENT_SUCCESSFUL.fullName());
-            completePayment(variables);
-
             //complete the document generation
             variables.putValue(
                 FLOW_STATE,
@@ -270,16 +226,6 @@ public class CreateClaimSpecTest extends BpmnBaseTest {
             //complete the start business process
             startBusinessProcess(variables);
 
-            //complete the case assignment process
-            completeCaseAssignment(variables);
-
-            //validate the fee
-            validateFee(variables);
-
-            //complete the payment
-            variables.putValue(FLOW_STATE, FlowState.CLAIM_ISSUED_PAYMENT_SUCCESSFUL.fullName());
-            completePayment(variables);
-
             //complete the document generation
             variables.putValue(
                 FLOW_STATE,
@@ -324,57 +270,6 @@ public class CreateClaimSpecTest extends BpmnBaseTest {
         }
 
         @Test
-        void shouldFailedCreateClaim_whenPaymentFailed() {
-            //assert process has started
-            assertFalse(processInstance.isEnded());
-
-            //assert message start event
-            assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
-
-            VariableMap variables = Variables.createVariables();
-            variables.put(FLOW_FLAGS, Map.of("RPA_CONTINUOUS_FEED", true));
-
-            //complete the start business process
-            startBusinessProcess(variables);
-
-            //complete the case assignment process
-            completeCaseAssignment(variables);
-
-            //validate the fee
-            validateFee(variables);
-
-            //complete the payment
-            variables.putValue(FLOW_STATE, FlowState.CLAIM_ISSUED_PAYMENT_FAILED.fullName());
-            completePayment(variables);
-
-            //payment failed
-            ExternalTask claimIssue = assertNextExternalTask(PROCESS_CASE_EVENT);
-            assertCompleteExternalTask(
-                claimIssue,
-                PROCESS_CASE_EVENT,
-                PROCESS_PAYMENT_FAILED_EVENT,
-                PROCESS_PAYMENT_FAILED_ACTIVITY_ID,
-                variables
-            );
-
-            //complete the Robotics notification
-            ExternalTask forRobotics = assertNextExternalTask(PROCESS_CASE_EVENT);
-            assertCompleteExternalTask(
-                forRobotics,
-                PROCESS_CASE_EVENT,
-                NOTIFY_APPLICANT_SOLICITOR1_FOR_FAILED_PAYMENT_SPEC_EVENT,
-                NOTIFY_APPLICANT_SOLICITOR1_FOR_FAILED_PAYMENT_SPEC_ACTIVITY_ID,
-                variables
-            );
-
-            //end business process
-            ExternalTask endBusinessProcess = assertNextExternalTask(END_BUSINESS_PROCESS);
-            completeBusinessProcess(endBusinessProcess);
-
-            assertNoExternalTasksLeft();
-        }
-
-        @Test
         void shouldSuccessfullyCompleteCreateClaim_whenClaimOnline() {
             //assert process has started
             assertFalse(processInstance.isEnded());
@@ -387,16 +282,6 @@ public class CreateClaimSpecTest extends BpmnBaseTest {
 
             //complete the start business process
             startBusinessProcess(variables);
-
-            //complete the case assignment process
-            completeCaseAssignment(variables);
-
-            //validate the fee
-            validateFee(variables);
-
-            //complete the payment
-            variables.putValue(FLOW_STATE, FlowState.CLAIM_ISSUED_PAYMENT_SUCCESSFUL.fullName());
-            completePayment(variables);
 
             //complete the document generation
             variables.putValue(
@@ -462,28 +347,6 @@ public class CreateClaimSpecTest extends BpmnBaseTest {
         }
     }
 
-    private void validateFee(VariableMap variables) {
-        ExternalTask feeTask = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            feeTask,
-            PROCESS_CASE_EVENT,
-            VALIDATE_FEE_EVENT,
-            VALIDATE_FEE_ACTIVITY_ID,
-            variables
-        );
-    }
-
-    private void completeCaseAssignment(VariableMap variables) {
-        ExternalTask caseAssignment = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            caseAssignment,
-            PROCESS_CASE_EVENT,
-            CASE_ASSIGNMENT_EVENT,
-            CASE_ASSIGNMENT_ACTIVITY_ID,
-            variables
-        );
-    }
-
     public void startBusinessProcess(VariableMap variables) {
         ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
         assertCompleteExternalTask(
@@ -491,17 +354,6 @@ public class CreateClaimSpecTest extends BpmnBaseTest {
             START_BUSINESS_TOPIC,
             START_BUSINESS_EVENT,
             START_BUSINESS_ACTIVITY,
-            variables
-        );
-    }
-
-    public void completePayment(VariableMap variables) {
-        ExternalTask paymentTask = assertNextExternalTask(PROCESS_PAYMENT_TOPIC);
-        assertCompleteExternalTask(
-            paymentTask,
-            PROCESS_PAYMENT_TOPIC,
-            MAKE_PBA_PAYMENT_EVENT,
-            MAKE_PAYMENT_ACTIVITY_ID,
             variables
         );
     }
@@ -517,3 +369,4 @@ public class CreateClaimSpecTest extends BpmnBaseTest {
         );
     }
 }
+
