7d06e3772621858c6173806ed2983cafbadb3189
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2022-11-29 10:09:40 +0000

    CIV-5743 add robotics notification

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/CreateClaimLipTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/CreateClaimLipTest.java
@@ -5,6 +5,8 @@ import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.Variables;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 public class CreateClaimLipTest extends BpmnBaseTest {
 
     private static final String FILE_NAME = "create_lip_claim.bpmn";
@@ -12,6 +14,8 @@ public class CreateClaimLipTest extends BpmnBaseTest {
     private static final String PROCESS_ID = "CREATE_LIP_CLAIM_PROCESS_ID";
     private static final String PROCESS_CLAIM_ISSUE_EVENT = "PROCESS_CLAIM_ISSUE_SPEC";
     private static final String PROCESS_CLAIM_ISSUE_ACTIVITY_ID = "IssueClaimForSpec";
+    private static final String NOTIFY_RPA_ON_CONTINUOUS_FEED_EVENT = "NOTIFY_RPA_ON_CONTINUOUS_FEED";
+    private static final String NOTIFY_RPA_ON_CONTINUOUS_FEED_ACTIVITY_ID = "NotifyRoboticsOnContinuousFeed";
 
     public CreateClaimLipTest() {
         super(FILE_NAME, PROCESS_ID);
@@ -21,8 +25,19 @@ public class CreateClaimLipTest extends BpmnBaseTest {
     void shouldSuccessfullyCreateLipClaim() {
         assertProcessStartedWithMessage(MESSAGE_NAME, PROCESS_ID);
         VariableMap variables = Variables.createVariables();
+        variables.put(FLOW_FLAGS, Map.of(
+            "RPA_CONTINUOUS_FEED", true,
+            "PIP_ENABLED", true));
         startBusinessProcess(variables);
         completeClaimIssue(variables);
+        ExternalTask forRobotics = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(
+            forRobotics,
+            PROCESS_CASE_EVENT,
+            NOTIFY_RPA_ON_CONTINUOUS_FEED_EVENT,
+            NOTIFY_RPA_ON_CONTINUOUS_FEED_ACTIVITY_ID,
+            variables
+        );
         completeBusinessProcess(assertNextExternalTask(END_BUSINESS_PROCESS));
     }
 
