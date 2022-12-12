857305700817dea05aaff9d0d48915132eedc470
Azam <muhammad.ghulam@hmcts.net>
2022-12-12 15:26:00 +0000

    CIV-6413: Added logic for not notifying unrepresented defendant1

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/CaseProceedsInCasemanTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/CaseProceedsInCasemanTest.java
@@ -4,6 +4,8 @@ import org.camunda.bpm.engine.externaltask.ExternalTask;
 import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.Variables;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Map;
 
@@ -167,6 +169,73 @@ class CaseProceedsInCasemanTest extends BpmnBaseTest {
         assertNoExternalTasksLeft();
     }
 
+    @ParameterizedTest
+    @CsvSource({"true", "false", "null"})
+    void shouldSuccessfullyComplete_unrepresentedDefendant(boolean unrepresentedDefendant1) {
+        //assert process has started
+        assertFalse(processInstance.isEnded());
+
+        //assert message start event
+        assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
+
+        VariableMap variables = Variables.createVariables();
+        variables.put("flowFlags", Map.of(
+            UNREPRESENTED_DEFENDANT_ONE, unrepresentedDefendant1,
+            GENERAL_APPLICATION_ENABLED, false
+        ));
+
+        //complete the start business process
+        ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
+        assertCompleteExternalTask(
+            startBusiness,
+            START_BUSINESS_TOPIC,
+            START_BUSINESS_EVENT,
+            START_BUSINESS_ACTIVITY,
+            variables
+        );
+
+        //Take offline
+        ExternalTask takeOffline = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(
+            takeOffline,
+            PROCESS_CASE_EVENT,
+            PROCEEDS_IN_HERITAGE_SYSTEM_EVENT,
+            PROCEEDS_IN_HERITAGE_SYSTEM_ACTIVITY_ID
+        );
+
+        //complete the RPA notification
+        ExternalTask rpaNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(
+            rpaNotification,
+            PROCESS_CASE_EVENT,
+            "NOTIFY_RPA_ON_CASE_HANDED_OFFLINE",
+            "NotifyRoboticsOnCaseHandedOffline"
+        );
+
+        //complete the notification to respondent 1
+        if (!unrepresentedDefendant1) {
+            ExternalTask respondentNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
+            assertCompleteExternalTask(respondentNotification,
+                                       PROCESS_CASE_EVENT,
+                                       "NOTIFY_RESPONDENT_SOLICITOR1_FOR_CASE_PROCEEDS_IN_CASEMAN",
+                                       "CaseProceedsInCasemanNotifyRespondentSolicitor1"
+            );
+        }
+
+        //complete the notification to applicant
+        ExternalTask applicantNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(applicantNotification,
+                                   PROCESS_CASE_EVENT,
+                                   "NOTIFY_APPLICANT_SOLICITOR1_FOR_CASE_PROCEEDS_IN_CASEMAN",
+                                   "CaseProceedsInCasemanNotifyApplicantSolicitor1"
+        );
+
+        //end business process
+        ExternalTask endBusinessProcess = assertNextExternalTask(END_BUSINESS_PROCESS);
+        completeBusinessProcess(endBusinessProcess);
+
+        assertNoExternalTasksLeft();
+    }
     @Test
     void shouldAbort_whenStartBusinessProcessThrowsAnError() {
         //assert process has started
