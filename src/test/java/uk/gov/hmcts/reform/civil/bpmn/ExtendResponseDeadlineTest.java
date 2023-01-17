d25850637f3c82f79f5057783db7343cafad4fe6
TatsyMaslova <tatiana.maslova@HMCTS.NET>
2023-01-17 09:33:59 +0000

    CIV-1916 formatting

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/ExtendResponseDeadlineTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/ExtendResponseDeadlineTest.java
@@ -17,7 +17,8 @@ public class ExtendResponseDeadlineTest extends BpmnBaseTest {
     private static final String NOTIFY_CLAIMANT_EVENT = "NOTIFY_CLAIMANT_CUI_FOR_DEADLINE_EXTENSION";
     private static final String NOTIFY_CLAIMANT_ACTIVITY_NAME = "DefendantResponseDeadlineExtensionNotifyClaimant";
     private static final String NOTIFY_LIP_DEFENDANT_EVENT = "NOTIFY_DEFENDANT_CUI_FOR_DEADLINE_EXTENSION";
-    private static final String NOTIFY_LIP_DEFENDANT_ACTIVITY_NAME = "DefendantResponseDeadlineExtensionNotifyDefendant";
+    private static final String NOTIFY_LIP_DEFENDANT_ACTIVITY_NAME
+        = "DefendantResponseDeadlineExtensionNotifyDefendant";
 
     public ExtendResponseDeadlineTest() {
         super("extend_response_deadline.bpmn", PROCESS_ID);
