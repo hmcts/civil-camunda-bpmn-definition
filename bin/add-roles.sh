f379c369753d570fd8b2e6857740242f87fabfa2
suvarnathekkoot <suvarna.thekkoot@HMCTS.NET>
2023-02-28 08:30:43 +0000

    Adding right role names

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -22,8 +22,8 @@
 ./bin/utils/ccd-add-role.sh "RES-SOL-TWO-UNSPEC-PROFILE"
 ./bin/utils/ccd-add-role.sh "RES-SOL-TWO-SPEC-PROFILE"
 ./bin/utils/ccd-add-role.sh "payment-access"
-./bin/utils/ccd-add-role.sh "hearings-admin"
-./bin/utils/ccd-add-role.sh "hearings-viewer"
+./bin/utils/ccd-add-role.sh "hearing-manager"
+./bin/utils/ccd-add-role.sh "hearing-viewer"
 
 roles=("solicitor" "systemupdate" "admin" "staff")
 for role in "${roles[@]}"
