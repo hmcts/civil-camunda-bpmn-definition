5258e81bf54ccd4737ec82a6d57c6f54d4583c58
sankaviv1 <sankavi.mohanraj@version1.com>
2023-04-20 09:37:48 +0100

    checkstyle fix

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/BreathingSpaceLiftedTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/BreathingSpaceLiftedTest.java
@@ -1,8 +1,6 @@
 package uk.gov.hmcts.reform.civil.bpmn;
 
 import org.camunda.bpm.engine.externaltask.ExternalTask;
-import org.camunda.bpm.engine.variable.VariableMap;
-import org.camunda.bpm.engine.variable.Variables;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
