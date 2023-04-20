a14136d6633d06336287f47de4aebe823106d7e1
Sherlyn Khaw <sherlyn.khaw@evoco.co.uk>
2023-04-20 10:14:46 +0100

    fix checkstyles

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/AddCaseNoteTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/AddCaseNoteTest.java
@@ -4,10 +4,6 @@ import org.camunda.bpm.engine.externaltask.ExternalTask;
 import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.Variables;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
