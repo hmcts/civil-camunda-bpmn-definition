e3ba032c50dd8ff2dae9772ebae08028c0fd98a7
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-05-02 14:29:59 +0100

    CIV-7357 mediation successful diagram

---
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/MediationSuccessfulTest.java
@@ -0,0 +1,23 @@
+package uk.gov.hmcts.reform.civil.bpmn;
+
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
+import org.junit.jupiter.api.Test;
+
+public class MediationSuccessfulTest extends BpmnBaseTest{
+    private static final String FILE_NAME = "mediation_successful.bpmn";
+    private static final String MESSAGE_NAME = "MEDIATION_SUCCESSFUL";
+    private static final String PROCESS_ID = "MEDIATION_SUCCESSFUL_ID";
+
+    public MediationSuccessfulTest() {
+        super(FILE_NAME, PROCESS_ID);
+    }
+
+    @Test
+    void shouldSubmitSuccessfulMediation(){
+        assertProcessStartedWithMessage(MESSAGE_NAME, PROCESS_ID);
+        VariableMap variables = Variables.createVariables();
+        startBusinessProcess(variables);
+        completeBusinessProcess(assertNextExternalTask(END_BUSINESS_PROCESS));
+    }
+}
