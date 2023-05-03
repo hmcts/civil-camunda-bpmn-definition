2fa6532cb2634e4e136a59ab0a6743beb62212bb
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-05-03 09:53:07 +0100

    CIV-7357 formatting

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/MediationSuccessfulTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/MediationSuccessfulTest.java
@@ -4,7 +4,7 @@ import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.Variables;
 import org.junit.jupiter.api.Test;
 
-public class MediationSuccessfulTest extends BpmnBaseTest{
+public class MediationSuccessfulTest extends BpmnBaseTest {
     private static final String FILE_NAME = "mediation_successful.bpmn";
     private static final String MESSAGE_NAME = "MEDIATION_SUCCESSFUL";
     private static final String PROCESS_ID = "MEDIATION_SUCCESSFUL_ID";
@@ -14,7 +14,7 @@ public class MediationSuccessfulTest extends BpmnBaseTest{
     }
 
     @Test
-    void shouldSubmitSuccessfulMediation(){
+    void shouldSubmitSuccessfulMediation() {
         assertProcessStartedWithMessage(MESSAGE_NAME, PROCESS_ID);
         VariableMap variables = Variables.createVariables();
         startBusinessProcess(variables);
