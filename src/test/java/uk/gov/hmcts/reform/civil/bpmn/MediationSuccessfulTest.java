807c092a3e5c31f248d2dadce8c0596301df67a8
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-05-03 09:57:52 +0100

    CIV-7357 formatting

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/MediationSuccessfulTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/MediationSuccessfulTest.java
@@ -5,6 +5,7 @@ import org.camunda.bpm.engine.variable.Variables;
 import org.junit.jupiter.api.Test;
 
 public class MediationSuccessfulTest extends BpmnBaseTest {
+
     private static final String FILE_NAME = "mediation_successful.bpmn";
     private static final String MESSAGE_NAME = "MEDIATION_SUCCESSFUL";
     private static final String PROCESS_ID = "MEDIATION_SUCCESSFUL_ID";
