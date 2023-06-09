f61eb2eb50af46ae4ad78bf82d3f0b71a2c53cd3
asthamalviya <104994907+asthamalviya@users.noreply.github.com>
2023-06-09 18:48:50 +0100

    CIV-0000 Stopping poller for the weekend (#632)
    * Stopping poller for the weekend
    
    * Stopping poller for the weekend test
    
    * Supression

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/PollingEventEmitterSchedulerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/PollingEventEmitterSchedulerTest.java
@@ -36,12 +36,12 @@ class PollingEventEmitterSchedulerTest extends BpmnBaseTest {
         assertThat(jobDefinitions).hasSize(1);
         assertThat(jobDefinitions.get(0).getJobType()).isEqualTo("timer-start-event");
 
-        String cronString = "0 0/5 * * * ?";
+        String cronString = "0 0/5 * 13/1 * ? *";
         assertThat(jobDefinitions.get(0).getJobConfiguration()).isEqualTo("CYCLE: " + cronString);
         assertCronTriggerFiresAtExpectedTime(
             new CronExpression(cronString),
-            LocalDateTime.of(2020, 1, 9, 0, 0, 0),
-            LocalDateTime.of(2020, 1, 9, 0, 5, 0)
+            LocalDateTime.of(2020, 1, 13, 0, 0, 0),
+            LocalDateTime.of(2020, 1, 13, 0, 5, 0)
         );
 
         //get external tasks
