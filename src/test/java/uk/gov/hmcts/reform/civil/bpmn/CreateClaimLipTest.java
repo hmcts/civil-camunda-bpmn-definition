e16ba112ae0f6dd414d470357b1cb893098c506a
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-07-31 09:30:37 +0100

    CIV-9915 remove activities that are not needed in creating lip claim process

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/CreateClaimLipTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/CreateClaimLipTest.java
@@ -10,20 +10,11 @@ public class CreateClaimLipTest extends BpmnBaseTest {
     private static final String FILE_NAME = "create_lip_claim.bpmn";
     private static final String MESSAGE_NAME = "CREATE_LIP_CLAIM";
     private static final String PROCESS_ID = "CREATE_LIP_CLAIM_PROCESS_ID";
-    private static final String PROCESS_CLAIM_ISSUE_EVENT = "PROCESS_CLAIM_ISSUE_SPEC";
-    private static final String PROCESS_CLAIM_ISSUE_ACTIVITY_ID = "IssueClaimForSpec";
 
-    //notify applicant 1
     private static final String NOTIFY_APPLICANT1_FOR_CLAIM_CONTINUING_ONLINE_SPEC_EVENT
         = "NOTIFY_APPLICANT1_FOR_CLAIM_CONTINUING_ONLINE_SPEC";
     private static final String NOTIFY_APPLICANT1_FOR_CLAIM_CONTINUING_ONLINE_SPEC_ACTIVITY_ID
         = "CreateClaimContinuingOnlineNotifyApplicant1ForSpec";
-
-    //notify respondent 1
-    private static final String NOTIFY_RESPONDENT1_FOR_CLAIM_CONTINUING_ONLINE_SPEC_EVENT
-        = "NOTIFY_RESPONDENT1_FOR_CLAIM_CONTINUING_ONLINE_SPEC";
-    private static final String NOTIFY_RESPONDENT1_FOR_CLAIM_CONTINUING_ONLINE_SPEC_ACTIVITY_ID
-        = "CreateClaimContinuingOnlineNotifyRespondent1ForSpec";
 
     public CreateClaimLipTest() {
         super(FILE_NAME, PROCESS_ID);
@@ -34,21 +25,11 @@ public class CreateClaimLipTest extends BpmnBaseTest {
         assertProcessStartedWithMessage(MESSAGE_NAME, PROCESS_ID);
         VariableMap variables = Variables.createVariables();
         startBusinessProcess(variables);
-        completeClaimIssue(variables);
+        assertApplicantNotificationCompleted();
         completeBusinessProcess(assertNextExternalTask(END_BUSINESS_PROCESS));
     }
 
-    private void completeClaimIssue(final VariableMap variables) {
-        ExternalTask claimIssue = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            claimIssue,
-            PROCESS_CASE_EVENT,
-            PROCESS_CLAIM_ISSUE_EVENT,
-            PROCESS_CLAIM_ISSUE_ACTIVITY_ID,
-            variables
-        );
-
-        //complete the applicant notification
+    private void assertApplicantNotificationCompleted() {
         ExternalTask notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
         assertCompleteExternalTask(
             notificationTask,
@@ -56,15 +37,5 @@ public class CreateClaimLipTest extends BpmnBaseTest {
             NOTIFY_APPLICANT1_FOR_CLAIM_CONTINUING_ONLINE_SPEC_EVENT,
             NOTIFY_APPLICANT1_FOR_CLAIM_CONTINUING_ONLINE_SPEC_ACTIVITY_ID
         );
-
-        //complete the respondent notification
-        ExternalTask notificationRespondentTask = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            notificationRespondentTask,
-            PROCESS_CASE_EVENT,
-            NOTIFY_RESPONDENT1_FOR_CLAIM_CONTINUING_ONLINE_SPEC_EVENT,
-            NOTIFY_RESPONDENT1_FOR_CLAIM_CONTINUING_ONLINE_SPEC_ACTIVITY_ID
-        );
     }
-
 }
