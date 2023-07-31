0214a6623d614858286e81674e9730677e6d5eb0
TatsyMaslova <95431510+TatsyMaslova@users.noreply.github.com>
2023-07-31 11:36:17 +0100

    CIV-9915 remove notification as there would be another notification during claim creation

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/CreateClaimLipTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/CreateClaimLipTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.civil.bpmn;
 
-import org.camunda.bpm.engine.externaltask.ExternalTask;
 import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.Variables;
 import org.junit.jupiter.api.Test;
@@ -11,11 +10,6 @@ public class CreateClaimLipTest extends BpmnBaseTest {
     private static final String MESSAGE_NAME = "CREATE_LIP_CLAIM";
     private static final String PROCESS_ID = "CREATE_LIP_CLAIM_PROCESS_ID";
 
-    private static final String NOTIFY_APPLICANT1_FOR_CLAIM_CONTINUING_ONLINE_SPEC_EVENT
-        = "NOTIFY_APPLICANT1_FOR_CLAIM_CONTINUING_ONLINE_SPEC";
-    private static final String NOTIFY_APPLICANT1_FOR_CLAIM_CONTINUING_ONLINE_SPEC_ACTIVITY_ID
-        = "CreateClaimContinuingOnlineNotifyApplicant1ForSpec";
-
     public CreateClaimLipTest() {
         super(FILE_NAME, PROCESS_ID);
     }
@@ -25,17 +19,6 @@ public class CreateClaimLipTest extends BpmnBaseTest {
         assertProcessStartedWithMessage(MESSAGE_NAME, PROCESS_ID);
         VariableMap variables = Variables.createVariables();
         startBusinessProcess(variables);
-        assertApplicantNotificationCompleted();
         completeBusinessProcess(assertNextExternalTask(END_BUSINESS_PROCESS));
-    }
-
-    private void assertApplicantNotificationCompleted() {
-        ExternalTask notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            notificationTask,
-            PROCESS_CASE_EVENT,
-            NOTIFY_APPLICANT1_FOR_CLAIM_CONTINUING_ONLINE_SPEC_EVENT,
-            NOTIFY_APPLICANT1_FOR_CLAIM_CONTINUING_ONLINE_SPEC_ACTIVITY_ID
-        );
     }
 }
