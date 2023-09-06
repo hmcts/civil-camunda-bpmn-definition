e198c5518588a0a4b8bf16c341567e6ba88dd002
ElifDenizEsea <61947470+ElifDenizEsea@users.noreply.github.com>
2023-09-06 18:01:18 +0100

    CIV-10128 Claimant Response test

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/ClaimantResponseCuiTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/ClaimantResponseCuiTest.java
@@ -1,7 +1,11 @@
 package uk.gov.hmcts.reform.civil.bpmn;
 
 import org.camunda.bpm.engine.externaltask.ExternalTask;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -9,18 +13,16 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 public class ClaimantResponseCuiTest extends BpmnBaseTest {
 
     private static final String MESSAGE_NAME = "CLAIMANT_RESPONSE_CUI";
-    private static final String PROCESS_ID = "CLAIMANT_RESPONSE_CUI_PROCESS_ID_CUI";
+    private static final String PROCESS_ID = "CLAIMANT_RESPONSE_CUI_PROCESS_ID";
+    private static final String JUDICIAL_REFERRAL_EVENT = "JUDICIAL_REFERRAL";
+    private static final String JUDICIAL_REFERRAL_ACTIVITY_ID = "JudicialReferral";
 
-    //CCD Case Event
-
-    //ACTIVITY IDs
-  public ClaimantResponseCuiTest() {
+    public ClaimantResponseCuiTest() {
         super(
             "claimant_response_cui.bpmn",
-            "CLAIMANT_RESPONSE_CUI_PROCESS_ID_CUI"
+            "CLAIMANT_RESPONSE_CUI_PROCESS_ID"
         );
     }
-
 
     @Test
     void shouldRunProcess() {
@@ -30,7 +32,30 @@ public class ClaimantResponseCuiTest extends BpmnBaseTest {
 
         //assert message start event
         assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
-
+        ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
+        VariableMap variables = Variables.createVariables();
+        variables.putValue("flowState", "MAIN.PART_ADMIT_NOT_SETTLED_NO_MEDIATION");
+        variables.put(FLOW_FLAGS, Map.of(
+            ONE_RESPONDENT_REPRESENTATIVE, true,
+            TWO_RESPONDENT_REPRESENTATIVES, false,
+            GENERAL_APPLICATION_ENABLED, true,
+            IS_MULTI_TRACK, true
+        ));
+        assertCompleteExternalTask(
+            startBusiness,
+            START_BUSINESS_TOPIC,
+            START_BUSINESS_EVENT,
+            START_BUSINESS_ACTIVITY,
+            variables
+        );
+        ExternalTask judicialReferral = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(
+            judicialReferral,
+            PROCESS_CASE_EVENT,
+            JUDICIAL_REFERRAL_EVENT,
+            JUDICIAL_REFERRAL_ACTIVITY_ID,
+            variables
+        );
         endBusinessProcess();
         assertNoExternalTasksLeft();
     }
