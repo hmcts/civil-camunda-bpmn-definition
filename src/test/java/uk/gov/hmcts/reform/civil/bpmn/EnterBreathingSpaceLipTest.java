42abb9b001bc4beebe9d09cd370da96f23547c05
jeswanth <jeswanth.reddy@hmcts.net>
2023-09-21 14:43:26 +0100

    CIV-6719 Notification--Debt Respite - Submit Function

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/EnterBreathingSpaceLipTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/EnterBreathingSpaceLipTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.civil.bpmn;
 
+import org.camunda.bpm.engine.externaltask.ExternalTask;
 import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.Variables;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,16 @@ public class EnterBreathingSpaceLipTest extends BpmnBaseTest {
     private static final String MESSAGE_NAME = "ENTER_BREATHING_SPACE_LIP";
     private static final String PROCESS_ID = "ENTER_BREATHING_SPACE_PROCESS_ID_LIP";
 
+    //CCD Case Event
+    private static final String NOTIFY_APPLICANT1_BREATHING_SPACE_LIP
+        = "NOTIFY_APPLICANT1_BREATHING_SPACE_LIP";
+    private static final String NOTIFY_RESPONDENT_BREATHING_SPACE_LIP
+        = "NOTIFY_RESPONDENT_BREATHING_SPACE_LIP";
+    //Activity IDs
+    private static final String NOTIFY_APPLICANT1_BREATHING_SPACE_LIP_ID
+        = "NotifyApplicant1BreathingSpaceLIP";
+    private static final String NOTIFY_RESPONDENT_BREATHING_SPACE_LIP_ID
+        = "NotifyRespondentBreathingSpaceLIP";
     public EnterBreathingSpaceLipTest() {
         super(FILE_NAME, PROCESS_ID);
     }
@@ -19,6 +30,26 @@ public class EnterBreathingSpaceLipTest extends BpmnBaseTest {
         assertProcessStartedWithMessage(MESSAGE_NAME, PROCESS_ID);
         VariableMap variables = Variables.createVariables();
         startBusinessProcess(variables);
+        notifyApplicant1BreathingSpaceLip();
+        notifyRespondentBreathingSpaceLip();
         completeBusinessProcess(assertNextExternalTask(END_BUSINESS_PROCESS));
+    }
+    private void notifyApplicant1BreathingSpaceLip() {
+        ExternalTask notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(
+            notificationTask,
+            PROCESS_CASE_EVENT,
+            NOTIFY_APPLICANT1_BREATHING_SPACE_LIP,
+            NOTIFY_APPLICANT1_BREATHING_SPACE_LIP_ID
+        );
+    }
+    private void notifyRespondentBreathingSpaceLip() {
+        ExternalTask notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(
+            notificationTask,
+            PROCESS_CASE_EVENT,
+            NOTIFY_RESPONDENT_BREATHING_SPACE_LIP,
+            NOTIFY_RESPONDENT_BREATHING_SPACE_LIP_ID
+        );
     }
 }
