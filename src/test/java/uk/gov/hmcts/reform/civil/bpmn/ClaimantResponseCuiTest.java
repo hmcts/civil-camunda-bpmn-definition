121a0631871736e7670e578aee2e880860f99f86
paul-pearson <paul.pearson@version1.com>
2024-01-22 16:18:04 +0000

    CIV-11543 Remove changes from ClaimantResponseCuiTest

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/ClaimantResponseCuiTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/ClaimantResponseCuiTest.java
@@ -16,7 +16,6 @@ public class ClaimantResponseCuiTest extends BpmnBaseTest {
     private static final String PROCESS_ID = "CLAIMANT_RESPONSE_CUI_PROCESS_ID";
     private static final String JUDICIAL_REFERRAL_EVENT = "JUDICIAL_REFERRAL";
     private static final String JUDICIAL_REFERRAL_ACTIVITY_ID = "JudicialReferral";
-    private static final String JUDICIAL_REFERRAL_FULL_DEFENCE_ACTIVITY_ID = "Judicial_Referral";
     //CCD Case Event
     private static final String NOTIFY_LIP_RESPONDENT_CLAIMANT_CONFIRM_TO_PROCEED
         = "NOTIFY_LIP_RESPONDENT_CLAIMANT_CONFIRM_TO_PROCEED";
@@ -50,12 +49,6 @@ public class ClaimantResponseCuiTest extends BpmnBaseTest {
 
     private static final String NOTIFY_CLAIMANT_FOR_RESPONDENT1_REJECT_REPAYMENT_ACTIVITY_ID
         = "ClaimantDisAgreeRepaymentPlanNotifyApplicant";
-    private static final String LIP_CLAIMANT_MD_ACTIVITY_ID = "Generate_LIP_Claimant_MD";
-    private static final String LIP_CLAIMANT_MD = "GENERATE_LIP_CLAIMANT_MANUAL_DETERMINATION";
-    private static final String GENERATE_INTERLOCUTORY_JUDGEMENT_DOCUMENT
-        = "GENERATE_INTERLOCUTORY_JUDGEMENT_DOCUMENT";
-    private static final String GENERATE_INTERLOCUTORY_JUDGEMENT_DOCUMENT_ACTIVITY_ID
-        = "GenerateInterlocutoryJudgementDocument";
 
     public ClaimantResponseCuiTest() {
         super(
@@ -208,8 +201,6 @@ public class ClaimantResponseCuiTest extends BpmnBaseTest {
 
         notifyRespondentClaimantRejectRepayment();
         notifyClaimantClaimantRejectRepayment();
-        generateManualDeterminationPdf();
-        requestInterlockJudgement();
         generateDQPdf();
         endBusinessProcess();
         assertNoExternalTasksLeft();
@@ -239,93 +230,6 @@ public class ClaimantResponseCuiTest extends BpmnBaseTest {
 
         notifyRespondentClaimantRejectRepayment();
         notifyClaimantClaimantRejectRepayment();
-        generateManualDeterminationPdf();
-        requestInterlockJudgement();
-        generateDQPdf();
-        endBusinessProcess();
-        assertNoExternalTasksLeft();
-    }
-
-    @Test
-    void shouldRunProcess_ClaimIsInPartAdmitPayImmediately() {
-
-        //assert process has started
-        assertFalse(processInstance.isEnded());
-
-        //assert message start event
-        assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
-        ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
-        VariableMap variables = Variables.createVariables();
-        variables.putValue("flowState", "MAIN.PART_ADMIT_PAY_IMMEDIATELY");
-        variables.put(FLOW_FLAGS, Map.of(
-                "LIP_JUDGMENT_ADMISSION", false
-        ));
-        assertCompleteExternalTask(
-                startBusiness,
-                START_BUSINESS_TOPIC,
-                START_BUSINESS_EVENT,
-                START_BUSINESS_ACTIVITY,
-                variables
-        );
-        notifyRespondentClaimantConfirmsToProceed();
-        notifyApplicantClaimantConfirmsToProceed();
-        generateDQPdf();
-        endBusinessProcess();
-        assertNoExternalTasksLeft();
-    }
-
-    @Test
-    void shouldRunProcess_ClaimIsInPartAdmitPayImmediately() {
-
-        //assert process has started
-        assertFalse(processInstance.isEnded());
-
-        //assert message start event
-        assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
-        ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
-        VariableMap variables = Variables.createVariables();
-        variables.putValue("flowState", "MAIN.PART_ADMIT_PAY_IMMEDIATELY");
-        variables.put(FLOW_FLAGS, Map.of(
-                "LIP_JUDGMENT_ADMISSION", false
-        ));
-        assertCompleteExternalTask(
-                startBusiness,
-                START_BUSINESS_TOPIC,
-                START_BUSINESS_EVENT,
-                START_BUSINESS_ACTIVITY,
-                variables
-        );
-        notifyRespondentClaimantConfirmsToProceed();
-        notifyApplicantClaimantConfirmsToProceed();
-        generateDQPdf();
-        endBusinessProcess();
-        assertNoExternalTasksLeft();
-    }
-
-    @Test
-    void shouldRunProcess_ClaimFullDefenceNotAgreeMediation() {
-        //Given
-        VariableMap variables = Variables.createVariables();
-        variables.putValue("flowState", "MAIN.FULL_DEFENCE_PROCEED");
-        variables.put(FLOW_FLAGS, Map.of(
-            ONE_RESPONDENT_REPRESENTATIVE, true,
-            TWO_RESPONDENT_REPRESENTATIVES, false,
-            GENERAL_APPLICATION_ENABLED, true,
-            IS_MULTI_TRACK, true
-        ));
-
-        //Then
-        assertProcessHasStarted();
-        assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
-        startBusinessProcess(variables);
-        assertCompletedCaseEvent(
-            JUDICIAL_REFERRAL_EVENT,
-            JUDICIAL_REFERRAL_FULL_DEFENCE_ACTIVITY_ID,
-            variables
-        );
-
-        notifyRespondentClaimantConfirmsToProceed();
-        notifyApplicantClaimantConfirmsToProceed();
         generateDQPdf();
         endBusinessProcess();
         assertNoExternalTasksLeft();
@@ -349,10 +253,6 @@ public class ClaimantResponseCuiTest extends BpmnBaseTest {
 
     private void generateRPAContinuousFeed() {
         assertCompletedCaseEvent(NOTIFY_RPA_ON_CONTINUOUS_FEED, NOTIFY_RPA_ON_CONTINUOUS_FEED_ACTIVITY_ID);
-    }
-
-    private void generateManualDeterminationPdf() {
-        assertCompletedCaseEvent(LIP_CLAIMANT_MD, LIP_CLAIMANT_MD_ACTIVITY_ID);
     }
 
     private void assertCompletedCaseEvent(String eventName, String activityId) {
@@ -383,13 +283,6 @@ public class ClaimantResponseCuiTest extends BpmnBaseTest {
             PROCESS_CASE_EVENT,
             NOTIFY_LIP_APPLICANT_CLAIMANT_CONFIRM_TO_PROCEED,
             NOTIFY_LIP_APPLICANT_CLAIMANT_CONFIRM_TO_PROCEED_ACTIVITY_ID
-        );
-    }
-
-    private void requestInterlockJudgement() {
-        assertCompletedCaseEvent(
-            GENERATE_INTERLOCUTORY_JUDGEMENT_DOCUMENT,
-            GENERATE_INTERLOCUTORY_JUDGEMENT_DOCUMENT_ACTIVITY_ID
         );
     }
 
