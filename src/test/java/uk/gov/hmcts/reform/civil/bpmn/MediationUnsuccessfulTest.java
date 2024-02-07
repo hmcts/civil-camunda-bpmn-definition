2b1ef8c83b384d30f777e504b42e07f37d65d354
Sherlyn Khaw <sherlyn.khaw@evoco.co.uk>
2024-02-07 16:19:36 +0000

    add mediation unsuccessful tests

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/MediationUnsuccessfulTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/MediationUnsuccessfulTest.java
@@ -4,7 +4,7 @@ import org.camunda.bpm.engine.externaltask.ExternalTask;
 import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.Variables;
 import org.junit.jupiter.api.Test;
-
+import java.util.Map;
 public class MediationUnsuccessfulTest extends BpmnBaseTest {
 
     private static final String FILE_NAME = "mediation_unsuccessful.bpmn";
@@ -14,21 +14,32 @@ public class MediationUnsuccessfulTest extends BpmnBaseTest {
         = "NOTIFY_MEDIATION_UNSUCCESSFUL_CLAIMANT_LR";
     private static final String NOTIFY_MEDIATION_UNSUCCESSFUL_DEFENDANT_LIP
         = "NOTIFY_MEDIATION_UNSUCCESSFUL_DEFENDANT_LIP";
+    private static final String NOTIFY_MEDIATION_UNSUCCESSFUL_DEFENDANT_1_LR
+        = "NOTIFY_MEDIATION_UNSUCCESSFUL_DEFENDANT_1_LR";
+    private static final String NOTIFY_MEDIATION_UNSUCCESSFUL_DEFENDANT_2_LR
+        = "NOTIFY_MEDIATION_UNSUCCESSFUL_DEFENDANT_2_LR";
 
     private static final String NOTIFY_MEDIATION_UNSUCCESSFUL_CLAIMANT_LR_ACTIVITY_ID
         = "SendMediationUnsuccessfulClaimantLR";
-
     private static final String NOTIFY_MEDIATION_UNSUCCESSFUL_DEFENDANT_LIP_ACTIVITY_ID
         = "SendMediationUnsuccessfulDefendantLIP";
+    private static final String NOTIFY_MEDIATION_UNSUCCESSFUL_DEFENDANT_SOLICITOR_1_ACTIVITY_ID
+        = "SendMediationUnsuccessfulDefendant1LR";
+    private static final String NOTIFY_MEDIATION_UNSUCCESSFUL_DEFENDANT_SOLICITOR_2_ACTIVITY_ID
+        = "SendMediationUnsuccessfulDefendant2LR";
 
     public MediationUnsuccessfulTest() {
         super(FILE_NAME, PROCESS_ID);
     }
 
     @Test
-    void shouldSubmitUnsuccessfulMediation() {
+    void shouldSubmitUnsuccessfulMediationForLip() {
         assertProcessStartedWithMessage(MESSAGE_NAME, PROCESS_ID);
         VariableMap variables = Variables.createVariables();
+        variables.put(FLOW_FLAGS, Map.of(
+            ONE_RESPONDENT_REPRESENTATIVE, false,
+            UNREPRESENTED_DEFENDANT_ONE, true
+        ));
         startBusinessProcess(variables);
 
         ExternalTask notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
@@ -44,6 +55,72 @@ public class MediationUnsuccessfulTest extends BpmnBaseTest {
                                    PROCESS_CASE_EVENT,
                                    NOTIFY_MEDIATION_UNSUCCESSFUL_DEFENDANT_LIP,
                                    NOTIFY_MEDIATION_UNSUCCESSFUL_DEFENDANT_LIP_ACTIVITY_ID,
+                                   variables
+        );
+
+        completeBusinessProcess(assertNextExternalTask(END_BUSINESS_PROCESS));
+    }
+
+    @Test
+    void shouldSubmitUnsuccessfulMediationFor1v1LR() {
+        assertProcessStartedWithMessage(MESSAGE_NAME, PROCESS_ID);
+        VariableMap variables = Variables.createVariables();
+        variables.put(FLOW_FLAGS, Map.of(
+            ONE_RESPONDENT_REPRESENTATIVE, true,
+            UNREPRESENTED_DEFENDANT_ONE, false
+        ));
+        startBusinessProcess(variables);
+
+        ExternalTask notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(notificationTask,
+                                   PROCESS_CASE_EVENT,
+                                   NOTIFY_MEDIATION_UNSUCCESSFUL_CLAIMANT_LR,
+                                   NOTIFY_MEDIATION_UNSUCCESSFUL_CLAIMANT_LR_ACTIVITY_ID,
+                                   variables
+        );
+
+        notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(notificationTask,
+                                   PROCESS_CASE_EVENT,
+                                   NOTIFY_MEDIATION_UNSUCCESSFUL_DEFENDANT_1_LR,
+                                   NOTIFY_MEDIATION_UNSUCCESSFUL_DEFENDANT_SOLICITOR_1_ACTIVITY_ID,
+                                   variables
+        );
+
+        completeBusinessProcess(assertNextExternalTask(END_BUSINESS_PROCESS));
+    }
+
+    @Test
+    void shouldSubmitUnsuccessfulMediationFor1v2LR() {
+        assertProcessStartedWithMessage(MESSAGE_NAME, PROCESS_ID);
+        VariableMap variables = Variables.createVariables();
+        variables.put(FLOW_FLAGS, Map.of(
+            TWO_RESPONDENT_REPRESENTATIVES, true,
+            UNREPRESENTED_DEFENDANT_ONE, false
+        ));
+        startBusinessProcess(variables);
+
+        ExternalTask notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(notificationTask,
+                                   PROCESS_CASE_EVENT,
+                                   NOTIFY_MEDIATION_UNSUCCESSFUL_CLAIMANT_LR,
+                                   NOTIFY_MEDIATION_UNSUCCESSFUL_CLAIMANT_LR_ACTIVITY_ID,
+                                   variables
+        );
+
+        notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(notificationTask,
+                                   PROCESS_CASE_EVENT,
+                                   NOTIFY_MEDIATION_UNSUCCESSFUL_DEFENDANT_1_LR,
+                                   NOTIFY_MEDIATION_UNSUCCESSFUL_DEFENDANT_SOLICITOR_1_ACTIVITY_ID,
+                                   variables
+        );
+
+        notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(notificationTask,
+                                   PROCESS_CASE_EVENT,
+                                   NOTIFY_MEDIATION_UNSUCCESSFUL_DEFENDANT_2_LR,
+                                   NOTIFY_MEDIATION_UNSUCCESSFUL_DEFENDANT_SOLICITOR_2_ACTIVITY_ID,
                                    variables
         );
 
