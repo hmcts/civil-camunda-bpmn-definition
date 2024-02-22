890786e98b7d63c180df0eff03aa815057da841b
Pijian Liao <Pijian.Liao@HMCTS.NET>
2024-02-22 16:01:29 +0000

    CIV-12127 not start until 2025

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/GADocUploadNotifySchedulerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/GADocUploadNotifySchedulerTest.java
@@ -37,12 +37,12 @@ public class GADocUploadNotifySchedulerTest extends BpmnBaseTest {
         assertThat(jobDefinitions).hasSize(1);
         assertThat(jobDefinitions.get(0).getJobType()).isEqualTo("timer-start-event");
 
-        String cronString = "0 0 23 ? * * *";
+        String cronString = "0 0 23 ? * * 2025";
         assertThat(jobDefinitions.get(0).getJobConfiguration()).isEqualTo("CYCLE: " + cronString);
         assertCronTriggerFiresAtExpectedTime(
                 new CronExpression(cronString),
-                LocalDateTime.of(2020, 1, 1, 23, 0, 0),
-                LocalDateTime.of(2020, 1, 2, 23, 0, 0)
+                LocalDateTime.of(2025, 1, 1, 23, 0, 0),
+                LocalDateTime.of(2025, 1, 2, 23, 0, 0)
         );
 
         //get external tasks
