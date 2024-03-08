1d81b4e835a4fd111d1b4152e28df49948420b22
dharmendra kumar <dharmendra.kumar@hmcts.net>
2024-03-08 18:07:16 +0000

    Change scheduled time to 00 hours

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/FullAdmitPayImmediatelyNoPaymentSchedulerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/FullAdmitPayImmediatelyNoPaymentSchedulerTest.java
@@ -11,11 +11,11 @@ import org.camunda.bpm.engine.management.JobDefinition;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.Test;
 
-class FullAdminPayImmediatelyNoPaymentSchedulerTest extends BpmnBaseTest {
+class FullAdmitPayImmediatelyNoPaymentSchedulerTest extends BpmnBaseTest {
 
     public static final String TOPIC_NAME = "FULL_ADMIT_PAY_IMMEDIATELY_NO_PAYMENT_CHECK";
 
-    public FullAdminPayImmediatelyNoPaymentSchedulerTest() {
+    public FullAdmitPayImmediatelyNoPaymentSchedulerTest() {
         super("full_admit_pay_immediately_no_payment_scheduler.bpmn", "FULL_ADMIT_PAY_IMMEDIATELY_NO_PAYMENT_SCHEDULER");
     }
 
@@ -34,12 +34,12 @@ class FullAdminPayImmediatelyNoPaymentSchedulerTest extends BpmnBaseTest {
         assertThat(jobDefinitions).hasSize(1);
         assertThat(jobDefinitions.get(0).getJobType()).isEqualTo("timer-start-event");
 
-        String cronString = "0 0 1 * * ?";
+        String cronString = "0 0 0 * * ?";
         assertThat(jobDefinitions.get(0).getJobConfiguration()).isEqualTo("CYCLE: " + cronString);
         assertCronTriggerFiresAtExpectedTime(
             new CronExpression(cronString),
-            LocalDateTime.of(2024, 11, 30, 1, 0, 0),
-            LocalDateTime.of(2024, 12, 1, 1, 0, 0)
+            LocalDateTime.of(2024, 11, 30, 0, 0, 0),
+            LocalDateTime.of(2024, 12, 1, 0, 0, 0)
         );
 
         //get external tasks
