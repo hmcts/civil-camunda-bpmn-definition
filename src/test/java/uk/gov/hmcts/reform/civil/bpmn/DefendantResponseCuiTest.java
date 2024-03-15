8fc076be9bb682548fd4d96636f263e4dbf9b9f2
meulendijksm <max.meulendijks@hmcts.net>
2024-03-15 13:03:10 +0000

    CIV-13119 - camunda changes for defendant response dashboard

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/DefendantResponseCuiTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/DefendantResponseCuiTest.java
@@ -24,6 +24,11 @@ public class DefendantResponseCuiTest extends BpmnBaseTest {
         = "NOTIFY_LIP_DEFENDANT_RESPONSE_SUBMISSION";
     private static final String GENERATE_RESPONSE_DQ_LIP_SEALED_PDF = "GENERATE_RESPONSE_DQ_LIP_SEALED";
     private static final String GENERATE_LIP_RESPONSE_PDF = "GENERATE_RESPONSE_CUI_SEALED";
+    private static final String GENERATE_CLAIMANT_DASHBOARD
+        = "CREATE_CLAIMANT_DASHBOARD_NOTIFICATION_FOR_DEFENDANT_RESPONSE";
+
+    private static final String GENERATE_DEFENDANT_DASHBOARD
+        = "CREATE_DEFENDANT_DASHBOARD_NOTIFICATION_FOR_DEFENDANT_RESPONSE";
 
     //ACTIVITY IDs
     private static final String NOTIFY_RESPONDENT_SOLICITOR_1_CONTACT_CHANGE_ACTIVITY_ID
@@ -34,6 +39,10 @@ public class DefendantResponseCuiTest extends BpmnBaseTest {
         = "DefendantLipResponseNotifyDefendant";
     private static final String GENERATE_LIP_DQ_PDF_ACTIVITY = "GenerateSealedLipDQPdf";
     private static final String GENERATE_LIP_RESPONSE_PDF_ACTIVITY = "GenerateSealedLipResponsePdf";
+    private static final String GENERATE_CLAIMANT_DASHBOARD_ACTIVITY
+        = "GenerateClaimantDashboardNotificationDefendantResponse";
+    private static final String GENERATE_DEFENDANT_DASHBOARD_ACTIVITY
+        = "GenerateDefendantDashboardNotificationDefendantResponse";
 
     public DefendantResponseCuiTest() {
         super(
@@ -63,6 +72,8 @@ public class DefendantResponseCuiTest extends BpmnBaseTest {
         verifyApplicantNotificationOfResponseSubmissionCompleted();
         verifySealedDQGenerationCompleted();
         verifySealedResponseGenerationCompleted();
+        verifyGenerateDashboardNotificationClaimant();
+        verifyGenerateDashboardNotificationDefendant();
 
         endBusinessProcess();
         assertNoExternalTasksLeft();
@@ -86,6 +97,8 @@ public class DefendantResponseCuiTest extends BpmnBaseTest {
         verifyApplicantNotificationOfResponseSubmissionCompleted();
         verifySealedDQGenerationCompleted();
         verifySealedResponseGenerationCompleted();
+        verifyGenerateDashboardNotificationClaimant();
+        verifyGenerateDashboardNotificationDefendant();
 
         endBusinessProcess();
         assertNoExternalTasksLeft();
@@ -108,6 +121,8 @@ public class DefendantResponseCuiTest extends BpmnBaseTest {
         verifyDefendantLipNotificationOfResponseSubmissionCompleted();
         verifySealedDQGenerationCompleted();
         verifySealedResponseGenerationCompleted();
+        verifyGenerateDashboardNotificationClaimant();
+        verifyGenerateDashboardNotificationDefendant();
 
         endBusinessProcess();
         assertNoExternalTasksLeft();
@@ -166,6 +181,20 @@ public class DefendantResponseCuiTest extends BpmnBaseTest {
             START_BUSINESS_EVENT,
             START_BUSINESS_ACTIVITY,
             variables
+        );
+    }
+
+    private void verifyGenerateDashboardNotificationClaimant() {
+        verifyTaskIsComplete(
+            GENERATE_CLAIMANT_DASHBOARD,
+            GENERATE_CLAIMANT_DASHBOARD_ACTIVITY
+        );
+    }
+
+    private void verifyGenerateDashboardNotificationDefendant() {
+        verifyTaskIsComplete(
+            GENERATE_DEFENDANT_DASHBOARD,
+            GENERATE_DEFENDANT_DASHBOARD_ACTIVITY
         );
     }
 
