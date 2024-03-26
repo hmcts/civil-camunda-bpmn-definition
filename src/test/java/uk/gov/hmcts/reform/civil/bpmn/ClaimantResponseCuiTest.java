7017a7b19cd51fdf155f7d9af06fbd66265f2f25
jeswanth <jeswanth.reddy@hmcts.net>
2024-03-26 18:08:12 +0000

    rebase with team2b

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/ClaimantResponseCuiTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/ClaimantResponseCuiTest.java
@@ -416,6 +416,8 @@ public class ClaimantResponseCuiTest extends BpmnBaseTest {
         notifyApplicantClaimantConfirmsToProceed();
         generateDQPdf();
         updateClaimState();
+        createClaimantDashboardNotification();
+        createDefendantDashboardNotification();
         endBusinessProcess();
         assertNoExternalTasksLeft();
     }
