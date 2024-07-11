a67aa523ecac5b2b5778d2b1fb43733317f34489
Pijian Liao <Pijian.Liao@HMCTS.NET>
2024-07-11 16:43:05 +0100

    CIV-12772 camunda flow for ga hwf notify

---
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/GaHwfNotifyTest.java
@@ -0,0 +1,77 @@
+package uk.gov.hmcts.reform.civil.bpmn;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static uk.gov.hmcts.reform.civil.bpmn.BpmnBaseJudgeGASpecTest.MAKE_DECISION_CASE_EVENT;
+
+import java.util.Map;
+
+import org.camunda.bpm.engine.externaltask.ExternalTask;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GaHwfNotifyTest extends BpmnBaseGASpecTest {
+
+    private static final String FILE_NAME = "notify_ga_applicant_hwf_outcome.bpmn";
+    private static final String MESSAGE_NAME = "NOTIFY_APPLICANT_LIP_HWF";
+    private static final String PROCESS_ID = "GA_NOTIFY_HWF";
+    private static final String ACTIVITY_ID = "Notify_App_Lip_Hwf";
+    private static final String START_BUSINESS_TOPIC = "START_GA_BUSINESS_PROCESS";
+    public static final String NOTIFY_EVENT = "applicationProcessCaseEventGASpec";
+
+    public GaHwfNotifyTest() {
+        super(FILE_NAME, PROCESS_ID);
+    }
+
+    @BeforeEach
+    void setup() {
+        //deploy process
+        startBusinessProcessDeployment = engine.getRepositoryService()
+                .createDeployment()
+                .addClasspathResource(String.format(DIAGRAM_PATH,
+                        "start_business_process_in_general_application.bpmn"))
+                .deploy();
+        endBusinessProcessDeployment = engine.getRepositoryService()
+                .createDeployment()
+                .addClasspathResource(String.format(DIAGRAM_PATH, "end_ga_business_process.bpmn"))
+                .deploy();
+        deployment = engine.getRepositoryService()
+                .createDeployment()
+                .addClasspathResource(String.format(DIAGRAM_PATH, bpmnFileName))
+                .deploy();
+        processInstance = engine.getRuntimeService().startProcessInstanceByKey(processId);
+    }
+
+    @Test
+    void shouldSuccessfullyCompleteCreatePDFDocument_whenCalled() {
+        //assert process has started
+        assertFalse(processInstance.isEnded());
+
+        //assert message start event
+        assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
+
+        //complete the start business process
+        ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
+        assertCompleteExternalTask(
+                startBusiness,
+                START_BUSINESS_TOPIC,
+                START_GA_BUSINESS_EVENT,
+                START_GA_BUSINESS_ACTIVITY
+        );
+        ExternalTask notifyTask = assertNextExternalTask(NOTIFY_EVENT);
+        assertCompleteExternalTask(
+                notifyTask,
+                NOTIFY_EVENT,
+                MESSAGE_NAME,
+                ACTIVITY_ID
+        );
+
+        //end business process
+        ExternalTask endBusinessProcess = assertNextExternalTask(END_GA_BUSINESS_PROCESS);
+        completeGaBusinessProcess(endBusinessProcess);
+
+        assertNoExternalTasksLeft();
+    }
+}
