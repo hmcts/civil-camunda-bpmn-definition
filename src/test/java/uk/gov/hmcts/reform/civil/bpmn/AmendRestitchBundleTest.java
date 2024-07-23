39e40f35017a95c872772d273983cd902971b22e
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-07-23 16:37:49 +0100

    CIV-14593 checkstyle error

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/AmendRestitchBundleTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/AmendRestitchBundleTest.java
@@ -12,67 +12,68 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class AmendRestitchBundleTest extends BpmnBaseTest {
 
-        public static final String MESSAGE_NAME = "AMEND_RESTITCH_BUNDLE";
-        public static final String PROCESS_ID = "AMEND_RESTITCH_BUNDLE";
+    public static final String MESSAGE_NAME = "AMEND_RESTITCH_BUNDLE";
+    public static final String PROCESS_ID = "AMEND_RESTITCH_BUNDLE";
 
-        //CCD CASE EVENT
-        public static final String NOTIFY_CLAIMANT_AMEND_RESTITCH_BUNDLE
-            = "NOTIFY_CLAIMANT_AMEND_RESTITCH_BUNDLE";
-        public static final String NOTIFY_DEFENDANT_AMEND_RESTITCH_BUNDLE
-            = "NOTIFY_DEFENDANT_AMEND_RESTITCH_BUNDLE";
+    //CCD CASE EVENT
+    public static final String NOTIFY_CLAIMANT_AMEND_RESTITCH_BUNDLE
+        = "NOTIFY_CLAIMANT_AMEND_RESTITCH_BUNDLE";
+    public static final String NOTIFY_DEFENDANT_AMEND_RESTITCH_BUNDLE
+        = "NOTIFY_DEFENDANT_AMEND_RESTITCH_BUNDLE";
 
-        //ACTIVITY IDs
-        private static final String NOTIFY_CLAIMANT_AMEND_RESTITCH_BUNDLE_ACTIVITY_ID
-            = "NotifyClaimantAmendRestitchBundle";
-        private static final String NOTIFY_DEFENDANT_AMEND_RESTITCH_BUNDLE_ACTIVITY_ID
-            = "NotifyDefendantAmendRestitchBundle";
+    //ACTIVITY IDs
+    private static final String NOTIFY_CLAIMANT_AMEND_RESTITCH_BUNDLE_ACTIVITY_ID
+        = "NotifyClaimantAmendRestitchBundle";
+    private static final String NOTIFY_DEFENDANT_AMEND_RESTITCH_BUNDLE_ACTIVITY_ID
+        = "NotifyDefendantAmendRestitchBundle";
 
-        public AmendRestitchBundleTest() {
-            super("amend_restitch_bundle.bpmn", PROCESS_ID);
-        }
+    public AmendRestitchBundleTest() {
+        super("amend_restitch_bundle.bpmn", PROCESS_ID);
+    }
 
-        @Test
-        void shouldSuccessfullyCompleteAmendRestitchBundle() {
-            //assert process has started
-            assertFalse(processInstance.isEnded());
+    @Test
+    void shouldSuccessfullyCompleteAmendRestitchBundle() {
+        //assert process has started
+        assertFalse(processInstance.isEnded());
 
-            //assert message start event
-            assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
+        //assert message start event
+        assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
 
-            VariableMap variables = Variables.createVariables();
-            variables.put("flowFlags", Map.of(
-                UNREPRESENTED_DEFENDANT_ONE, false,
-                DASHBOARD_SERVICE_ENABLED, true,
-                CASE_PROGRESSION_ENABLED, true));
+        VariableMap variables = Variables.createVariables();
+        variables.put("flowFlags", Map.of(
+            UNREPRESENTED_DEFENDANT_ONE, false,
+            DASHBOARD_SERVICE_ENABLED, true,
+            CASE_PROGRESSION_ENABLED, true
+        ));
 
-            //complete the start business process
-            ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
-            assertCompleteExternalTask(startBusiness, START_BUSINESS_TOPIC,
-                                       START_BUSINESS_EVENT, START_BUSINESS_ACTIVITY, variables);
+        //complete the start business process
+        ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
+        assertCompleteExternalTask(startBusiness, START_BUSINESS_TOPIC,
+                                   START_BUSINESS_EVENT, START_BUSINESS_ACTIVITY, variables
+        );
 
-            ExternalTask notificationTask;
+        ExternalTask notificationTask;
 
-                //complete the claimant notification
-                notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
-                assertCompleteExternalTask(notificationTask, PROCESS_CASE_EVENT,
-                                           NOTIFY_CLAIMANT_AMEND_RESTITCH_BUNDLE,
-                                           NOTIFY_CLAIMANT_AMEND_RESTITCH_BUNDLE_ACTIVITY_ID,
-                                           variables
-                );
+        //complete the claimant notification
+        notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(notificationTask, PROCESS_CASE_EVENT,
+                                   NOTIFY_CLAIMANT_AMEND_RESTITCH_BUNDLE,
+                                   NOTIFY_CLAIMANT_AMEND_RESTITCH_BUNDLE_ACTIVITY_ID,
+                                   variables
+        );
 
+        //complete the defendant notification
+        notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(notificationTask, PROCESS_CASE_EVENT,
+                                   NOTIFY_DEFENDANT_AMEND_RESTITCH_BUNDLE,
+                                   NOTIFY_DEFENDANT_AMEND_RESTITCH_BUNDLE_ACTIVITY_ID,
+                                   variables
+        );
 
-            //complete the defendant notification
-            notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
-            assertCompleteExternalTask(notificationTask, PROCESS_CASE_EVENT,
-                                       NOTIFY_DEFENDANT_AMEND_RESTITCH_BUNDLE,
-                                       NOTIFY_DEFENDANT_AMEND_RESTITCH_BUNDLE_ACTIVITY_ID,
-                                       variables
-            );
+        //end business process
+        ExternalTask endBusinessProcess = assertNextExternalTask(END_BUSINESS_PROCESS);
+        completeBusinessProcess(endBusinessProcess);
 
-            //end business process
-            ExternalTask endBusinessProcess = assertNextExternalTask(END_BUSINESS_PROCESS);
-            completeBusinessProcess(endBusinessProcess);
-
-            assertNoExternalTasksLeft();
-        }
+        assertNoExternalTasksLeft();
+    }
 }
