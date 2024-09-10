305b8c3e64d2da35a93a038624c8f7beccecb411
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2024-09-10 12:57:31 +0100

    add test and yes condition

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/GaHearingScheduledTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/GaHearingScheduledTest.java
@@ -1,7 +1,11 @@
 package uk.gov.hmcts.reform.civil.bpmn;
 
 import org.camunda.bpm.engine.externaltask.ExternalTask;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -22,6 +26,16 @@ class GaHearingScheduledTest extends BpmnBaseHearingScheduledGATest {
     private static final String NOTIFY_HEARING_NOTICE_DEFENDANT_EVENT = "NOTIFY_HEARING_NOTICE_DEFENDANT";
     private static final String NOTIFY_HEARING_NOTICE_DEFENDANT_ACTIVITY_ID = "NotifyHearingNoticeDefendant";
 
+    private static final String CREATE_APPLICANT_DASHBOARD_NOTIFICATION_FOR_MAKE_DECISION = "CREATE_APPLICANT_DASHBOARD_NOTIFICATION_FOR_MAKE_DECISION";
+    private static final String CREATE_APPLICANT_DASHBOARD_NOTIFICATION_FOR_MAKE_DECISION_ACTIVITY_ID
+        = "hearingScheduledCreateDashboardNotificationForApplicant";
+
+    private static final String CREATE_RESPONDENT_DASHBOARD_NOTIFICATION_FOR_MAKE_DECISION = "CREATE_RESPONDENT_DASHBOARD_NOTIFICATION_FOR_MAKE_DECISION";
+    private static final String CREATE_RESPONDENT_DASHBOARD_NOTIFICATION_FOR_MAKE_DECISION_ACTIVITY_ID
+        = "hearingScheduledCreateDashboardNotificationForRespondent";
+
+    private static final String LIP_APPLICANT = "LIP_APPLICANT";
+    private static final String LIP_RESPONDENT = "LIP_RESPONDENT";
     public GaHearingScheduledTest() {
         super("ga_hearing_scheduled_access.bpmn", PROCESS_ID);
     }
@@ -30,6 +44,12 @@ class GaHearingScheduledTest extends BpmnBaseHearingScheduledGATest {
     void shouldSuccessfullyCompleteCreatePDFDocument_whenCalled() {
         //assert process has started
         assertFalse(processInstance.isEnded());
+
+        VariableMap variables = Variables.createVariables();
+        variables.put("flowFlags", Map.of(
+            LIP_APPLICANT, false,
+            LIP_RESPONDENT, false
+        ));
 
         //assert message start event
         assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
@@ -40,7 +60,8 @@ class GaHearingScheduledTest extends BpmnBaseHearingScheduledGATest {
             startBusiness,
             START_BUSINESS_TOPIC,
             START_BUSINESS_EVENT,
-            START_BUSINESS_ACTIVITY
+            START_BUSINESS_ACTIVITY,
+            variables
         );
         //Generate Hearing Notice Document
         ExternalTask generateHearingNoticeDocument = assertNextExternalTask(APPLICATION_PROCESS_CASE_EVENT);
@@ -48,7 +69,8 @@ class GaHearingScheduledTest extends BpmnBaseHearingScheduledGATest {
             generateHearingNoticeDocument,
             APPLICATION_PROCESS_CASE_EVENT,
             GENERATE_HEARING_NOTICE_EVENT,
-            GENERATE_HEARING_FORM_ACTIVITY_ID
+            GENERATE_HEARING_FORM_ACTIVITY_ID,
+            variables
         );
 
         //Link Document to main case event
@@ -57,7 +79,8 @@ class GaHearingScheduledTest extends BpmnBaseHearingScheduledGATest {
             addDocumentToMainCase,
             UPDATE_FROM_GA_CASE_EVENT,
             ADD_PDF_EVENT,
-            ADD_PDF_ID
+            ADD_PDF_ID,
+            variables
         );
 
         //Notify Hearing Notice Claimant
@@ -66,7 +89,8 @@ class GaHearingScheduledTest extends BpmnBaseHearingScheduledGATest {
             notifyHearingNoticeClaimant,
             PROCESS_EXTERNAL_CASE_EVENT,
             NOTIFY_HEARING_NOTICE_CLAIMANT_EVENT,
-            NOTIFY_HEARING_NOTICE_CLAIMANT_ACTIVITY_ID
+            NOTIFY_HEARING_NOTICE_CLAIMANT_ACTIVITY_ID,
+            variables
         );
 
         //Notify Hearing Notice Defendant(s)
@@ -75,7 +99,178 @@ class GaHearingScheduledTest extends BpmnBaseHearingScheduledGATest {
             notifyHearingNoticeDefendant,
             PROCESS_EXTERNAL_CASE_EVENT,
             NOTIFY_HEARING_NOTICE_DEFENDANT_EVENT,
-            NOTIFY_HEARING_NOTICE_DEFENDANT_ACTIVITY_ID
+            NOTIFY_HEARING_NOTICE_DEFENDANT_ACTIVITY_ID,
+            variables
+        );
+
+        //end business process
+        ExternalTask endBusinessProcess = assertNextExternalTask(END_BUSINESS_PROCESS);
+        completeBusinessProcess(endBusinessProcess);
+
+        assertNoExternalTasksLeft();
+    }
+
+    @Test
+    void shouldSuccessfullyCompleteCreatePDFDocumentForLIPvLIP_whenCalled() {
+        //assert process has started
+        assertFalse(processInstance.isEnded());
+
+        VariableMap variables = Variables.createVariables();
+        variables.put("flowFlags", Map.of(
+            LIP_APPLICANT, true,
+            LIP_RESPONDENT, true
+        ));
+
+        //assert message start event
+        assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
+
+        //complete the start business process
+        ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
+        assertCompleteExternalTask(
+            startBusiness,
+            START_BUSINESS_TOPIC,
+            START_BUSINESS_EVENT,
+            START_BUSINESS_ACTIVITY,
+            variables
+        );
+        //Generate Hearing Notice Document
+        ExternalTask generateHearingNoticeDocument = assertNextExternalTask(APPLICATION_PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(
+            generateHearingNoticeDocument,
+            APPLICATION_PROCESS_CASE_EVENT,
+            GENERATE_HEARING_NOTICE_EVENT,
+            GENERATE_HEARING_FORM_ACTIVITY_ID,
+            variables
+        );
+
+        //Link Document to main case event
+        ExternalTask addDocumentToMainCase = assertNextExternalTask(UPDATE_FROM_GA_CASE_EVENT);
+        assertCompleteExternalTask(
+            addDocumentToMainCase,
+            UPDATE_FROM_GA_CASE_EVENT,
+            ADD_PDF_EVENT,
+            ADD_PDF_ID,
+            variables
+        );
+
+        //Notify Hearing Notice Claimant
+        ExternalTask notifyHearingNoticeClaimant = assertNextExternalTask(PROCESS_EXTERNAL_CASE_EVENT);
+        assertCompleteExternalTask(
+            notifyHearingNoticeClaimant,
+            PROCESS_EXTERNAL_CASE_EVENT,
+            NOTIFY_HEARING_NOTICE_CLAIMANT_EVENT,
+            NOTIFY_HEARING_NOTICE_CLAIMANT_ACTIVITY_ID,
+            variables
+        );
+
+        //Notify Hearing Notice Defendant(s)
+        ExternalTask notifyHearingNoticeDefendant = assertNextExternalTask(PROCESS_EXTERNAL_CASE_EVENT);
+        assertCompleteExternalTask(
+            notifyHearingNoticeDefendant,
+            PROCESS_EXTERNAL_CASE_EVENT,
+            NOTIFY_HEARING_NOTICE_DEFENDANT_EVENT,
+            NOTIFY_HEARING_NOTICE_DEFENDANT_ACTIVITY_ID,
+            variables
+        );
+
+        //dashboard Hearing Notice Applicant
+        ExternalTask dashboardNotificationTaskApplicant = assertNextExternalTask(PROCESS_EXTERNAL_CASE_EVENT);
+        assertCompleteExternalTask(
+            dashboardNotificationTaskApplicant,
+            PROCESS_EXTERNAL_CASE_EVENT,
+            CREATE_APPLICANT_DASHBOARD_NOTIFICATION_FOR_MAKE_DECISION,
+            CREATE_APPLICANT_DASHBOARD_NOTIFICATION_FOR_MAKE_DECISION_ACTIVITY_ID,
+            variables
+        );
+
+        //dashboard Hearing Notice Defendant
+        ExternalTask dashboardNotificationTaskDefendant = assertNextExternalTask(PROCESS_EXTERNAL_CASE_EVENT);
+        assertCompleteExternalTask(
+            dashboardNotificationTaskDefendant,
+            PROCESS_EXTERNAL_CASE_EVENT,
+            CREATE_RESPONDENT_DASHBOARD_NOTIFICATION_FOR_MAKE_DECISION,
+            CREATE_RESPONDENT_DASHBOARD_NOTIFICATION_FOR_MAKE_DECISION_ACTIVITY_ID,
+            variables
+        );
+
+        //end business process
+        ExternalTask endBusinessProcess = assertNextExternalTask(END_BUSINESS_PROCESS);
+        completeBusinessProcess(endBusinessProcess);
+
+        assertNoExternalTasksLeft();
+    }
+
+    @Test
+    void shouldSuccessfullyCompleteCreatePDFDocumentForLRvLIP_whenCalled() {
+        //assert process has started
+        assertFalse(processInstance.isEnded());
+
+        VariableMap variables = Variables.createVariables();
+        variables.put("flowFlags", Map.of(
+            LIP_APPLICANT, false,
+            LIP_RESPONDENT, true
+        ));
+
+        //assert message start event
+        assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
+
+        //complete the start business process
+        ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
+        assertCompleteExternalTask(
+            startBusiness,
+            START_BUSINESS_TOPIC,
+            START_BUSINESS_EVENT,
+            START_BUSINESS_ACTIVITY,
+            variables
+        );
+        //Generate Hearing Notice Document
+        ExternalTask generateHearingNoticeDocument = assertNextExternalTask(APPLICATION_PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(
+            generateHearingNoticeDocument,
+            APPLICATION_PROCESS_CASE_EVENT,
+            GENERATE_HEARING_NOTICE_EVENT,
+            GENERATE_HEARING_FORM_ACTIVITY_ID,
+            variables
+        );
+
+        //Link Document to main case event
+        ExternalTask addDocumentToMainCase = assertNextExternalTask(UPDATE_FROM_GA_CASE_EVENT);
+        assertCompleteExternalTask(
+            addDocumentToMainCase,
+            UPDATE_FROM_GA_CASE_EVENT,
+            ADD_PDF_EVENT,
+            ADD_PDF_ID,
+            variables
+        );
+
+        //Notify Hearing Notice Claimant
+        ExternalTask notifyHearingNoticeClaimant = assertNextExternalTask(PROCESS_EXTERNAL_CASE_EVENT);
+        assertCompleteExternalTask(
+            notifyHearingNoticeClaimant,
+            PROCESS_EXTERNAL_CASE_EVENT,
+            NOTIFY_HEARING_NOTICE_CLAIMANT_EVENT,
+            NOTIFY_HEARING_NOTICE_CLAIMANT_ACTIVITY_ID,
+            variables
+        );
+
+        //Notify Hearing Notice Defendant(s)
+        ExternalTask notifyHearingNoticeDefendant = assertNextExternalTask(PROCESS_EXTERNAL_CASE_EVENT);
+        assertCompleteExternalTask(
+            notifyHearingNoticeDefendant,
+            PROCESS_EXTERNAL_CASE_EVENT,
+            NOTIFY_HEARING_NOTICE_DEFENDANT_EVENT,
+            NOTIFY_HEARING_NOTICE_DEFENDANT_ACTIVITY_ID,
+            variables
+        );
+
+        //dashboard Hearing Notice Defendant
+        ExternalTask dashboardNotificationTaskDefendant = assertNextExternalTask(PROCESS_EXTERNAL_CASE_EVENT);
+        assertCompleteExternalTask(
+            dashboardNotificationTaskDefendant,
+            PROCESS_EXTERNAL_CASE_EVENT,
+            CREATE_RESPONDENT_DASHBOARD_NOTIFICATION_FOR_MAKE_DECISION,
+            CREATE_RESPONDENT_DASHBOARD_NOTIFICATION_FOR_MAKE_DECISION_ACTIVITY_ID,
+            variables
         );
 
         //end business process
