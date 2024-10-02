0a10cfff5801be21253b6a1e996b964b5ead16f6
jeswanth <jeswanth.reddy@hmcts.net>
2024-10-02 14:34:53 +0100

    CIV-15151 added camunda flow for notifyinf

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/NotifyGATranslatedUploadedDocumentsTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/NotifyGATranslatedUploadedDocumentsTest.java
@@ -1,18 +1,51 @@
 package uk.gov.hmcts.reform.civil.bpmn;
 
 import org.camunda.bpm.engine.externaltask.ExternalTask;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-public class NotifyGATranslatedUploadedDocumentsTest extends BpmnBaseTest {
+public class NotifyGATranslatedUploadedDocumentsTest extends BpmnBaseGASpecTest {
 
+    public static final String NOTIFY_EVENT = "processExternalCaseEventGASpec";
+    private static final String START_BUSINESS_TOPIC = "START_GA_BUSINESS_PROCESS";
+    private static final String LIP_APPLICANT = "LIP_APPLICANT";
+    private static final String LIP_RESPONDENT = "LIP_RESPONDENT";
     public NotifyGATranslatedUploadedDocumentsTest() {
         super("upload_translated_document_ga_lip_notify.bpmn", "UPLOAD_TRANSLATED_DOCUMENT_GA_LIP_ID");
     }
 
+    @BeforeEach
+    void setup() {
+        //deploy process
+        startBusinessProcessDeployment = engine.getRepositoryService()
+            .createDeployment()
+            .addClasspathResource(String.format(DIAGRAM_PATH,
+                                                "start_business_process_in_general_application.bpmn"))
+            .deploy();
+        endBusinessProcessDeployment = engine.getRepositoryService()
+            .createDeployment()
+            .addClasspathResource(String.format(DIAGRAM_PATH, "end_general_application_business_process.bpmn"))
+            .deploy();
+        deployment = engine.getRepositoryService()
+            .createDeployment()
+            .addClasspathResource(String.format(DIAGRAM_PATH, bpmnFileName))
+            .deploy();
+        processInstance = engine.getRuntimeService().startProcessInstanceByKey(processId);
+    }
+
     @Test
     void shouldNotifyTranslatedDocumentUploaded() {
+        VariableMap variables = Variables.createVariables();
+        variables.put("flowFlags", Map.of(
+            LIP_APPLICANT, false,
+            LIP_RESPONDENT, false));
+
         //assert process has started
         assertFalse(processInstance.isEnded());
         //complete the start business process
@@ -20,24 +53,27 @@ public class NotifyGATranslatedUploadedDocumentsTest extends BpmnBaseTest {
         assertCompleteExternalTask(
             startBusiness,
             START_BUSINESS_TOPIC,
-            START_BUSINESS_EVENT,
-            START_BUSINESS_ACTIVITY
+            START_GA_BUSINESS_EVENT,
+            START_GA_BUSINESS_ACTIVITY,
+            variables
         );
         //complete the applicant notification
-        ExternalTask notificationApplicantTask = assertNextExternalTask(PROCESS_CASE_EVENT);
+        ExternalTask notificationApplicantTask = assertNextExternalTask(NOTIFY_EVENT);
         assertCompleteExternalTask(
             notificationApplicantTask,
-            PROCESS_CASE_EVENT,
+            NOTIFY_EVENT,
             "NOTIFY_APPLICANT_TRANSLATED_DOCUMENT_UPLOADED_GA",
-            "NotifyTranslatedDocumentUploadedToApplicantGA"
+            "NotifyTranslatedDocumentUploadedToApplicantGA",
+            variables
         );
         //complete the respondent notification
-        ExternalTask notificationRespondentTask = assertNextExternalTask(PROCESS_CASE_EVENT);
+        ExternalTask notificationRespondentTask = assertNextExternalTask(NOTIFY_EVENT);
         assertCompleteExternalTask(
             notificationRespondentTask,
-            PROCESS_CASE_EVENT,
+            NOTIFY_EVENT,
             "NOTIFY_RESPONDENT_TRANSLATED_DOCUMENT_UPLOADED_GA",
-            "NotifyTranslatedDocumentUploadedToRespondentGA"
+            "NotifyTranslatedDocumentUploadedToRespondentGA",
+            variables
         );
 
         //end business process
