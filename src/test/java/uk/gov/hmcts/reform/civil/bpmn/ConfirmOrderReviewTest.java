14f4655cd3f07a584452ea1aa7a7debef5b4081f
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2024-12-02 13:12:11 +0000

    CIV-15612 lint

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/ConfirmOrderReviewTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/ConfirmOrderReviewTest.java
@@ -5,8 +5,6 @@ import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.Variables;
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
