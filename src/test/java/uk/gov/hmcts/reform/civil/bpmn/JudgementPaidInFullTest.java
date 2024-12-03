85a07e37a2eb90c63e79ade038eea4e98ae5f4e3
Sherlyn Khaw <sherlyn.khaw@evoco.co.uk>
2024-12-03 08:45:12 +0000

    moved rpa to judgement paid in full

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/JudgementPaidInFullTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/JudgementPaidInFullTest.java
@@ -1,8 +1,13 @@
 package uk.gov.hmcts.reform.civil.bpmn;
 
 import org.camunda.bpm.engine.externaltask.ExternalTask;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
@@ -12,18 +17,26 @@ class JudgementPaidInFullTest extends BpmnBaseTest {
     public static final String PROCESS_ID = "JUDGMENT_PAID_IN_FULL";
     private static final String SEND_JUDGMENT_DETAILS_CJES = "SEND_JUDGMENT_DETAILS_CJES";
     private static final String SEND_JUDGMENT_DETAILS_CJES_ACTIVITY_ID = "SendJudgmentDetailsToCJES";
+    private static final String NOTIFY_RPA = "NOTIFY_RPA_ON_CONTINUOUS_FEED";
+    private static final String NOTIFY_RPA_ACTIVITY_ID = "NotifyRPA";
 
     public JudgementPaidInFullTest() {
         super("judgement_paid_in_full.bpmn", PROCESS_ID);
     }
 
-    @Test
-    void shouldSuccessfullyCompleteJudgmentPaidInFull_whenCalled() {
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    void shouldSuccessfullyCompleteJudgmentPaidInFull_whenCalled(Boolean joFlag) {
         //assert process has started
         assertFalse(processInstance.isEnded());
 
         //assert message start event
         assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
+
+        VariableMap variables = Variables.createVariables();
+        variables.put("flowFlags", Map.of(
+            IS_JO_LIVE_FEED_ACTIVE, joFlag
+        ));
 
         //complete the start business process
         ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
@@ -31,7 +44,8 @@ class JudgementPaidInFullTest extends BpmnBaseTest {
             startBusiness,
             START_BUSINESS_TOPIC,
             START_BUSINESS_EVENT,
-            START_BUSINESS_ACTIVITY
+            START_BUSINESS_ACTIVITY,
+            variables
         );
 
         //complete the Robotics notification
@@ -40,8 +54,20 @@ class JudgementPaidInFullTest extends BpmnBaseTest {
             forRobotics,
             PROCESS_CASE_EVENT,
             SEND_JUDGMENT_DETAILS_CJES,
-            SEND_JUDGMENT_DETAILS_CJES_ACTIVITY_ID
+            SEND_JUDGMENT_DETAILS_CJES_ACTIVITY_ID,
+            variables
         );
+
+        if (joFlag) {
+            ExternalTask notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
+            assertCompleteExternalTask(
+                notificationTask,
+                PROCESS_CASE_EVENT,
+                NOTIFY_RPA,
+                NOTIFY_RPA_ACTIVITY_ID,
+                variables
+            );
+        }
 
         //end business process
         ExternalTask endBusinessProcess = assertNextExternalTask(END_BUSINESS_PROCESS);
