044231d25bf11dc28b34a276d359ea00d6dc6e21
Sherlyn Khaw <sherlyn.khaw@evoco.co.uk>
2024-12-11 17:13:11 +0000

    remove duplicate code

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/BpmnBaseTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/BpmnBaseTest.java
@@ -50,7 +50,6 @@ public abstract class BpmnBaseTest {
     public static final String LIP_JUDGMENT_ADMISSION = "LIP_JUDGMENT_ADMISSION";
     public static final String CLAIM_ISSUE_BILINGUAL = "CLAIM_ISSUE_BILINGUAL";
     public static final String JO_ONLINE_LIVE_ENABLED = "JO_ONLINE_LIVE_ENABLED";
-    public static final String IS_JO_LIVE_FEED_ACTIVE = "IS_JO_LIVE_FEED_ACTIVE";
     public static final String DASHBOARD_SERVICE_ENABLED = "DASHBOARD_SERVICE_ENABLED";
     public static final String MINTI_ENABLED = "MINTI_ENABLED";
     public static final String CASE_PROGRESSION_ENABLED = "CASE_PROGRESSION_ENABLED";
