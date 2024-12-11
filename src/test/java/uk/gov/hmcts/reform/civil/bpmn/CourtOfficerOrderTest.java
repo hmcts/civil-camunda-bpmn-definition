bc62da697ee91d75f6f31f6bc2009aa92fc09a17
meulendijksm <max.meulendijks@hmcts.net>
2024-12-11 14:55:50 +0000

    CIV-15646 - remove unnecessary check for case progression

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/CourtOfficerOrderTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/CourtOfficerOrderTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.civil.bpmn;
 import org.camunda.bpm.engine.externaltask.ExternalTask;
 import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.Variables;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -37,6 +36,7 @@ public class CourtOfficerOrderTest extends BpmnBaseTest {
         = "GenerateOrderNotifyApplicantCourtOfficerOrderSolicitor1";
     public static final String NOTIFY_RESPONDENT_SOLICITOR1_FOR_COURT_OFFICER_ORDER_ACTIVITY_ID
         = "GenerateOrderNotifyRespondentCourtOfficerOrderSolicitor1";
+
     public CourtOfficerOrderTest() {
         super("court_officer_order.bpmn", PROCESS_ID);
     }
@@ -83,47 +83,6 @@ public class CourtOfficerOrderTest extends BpmnBaseTest {
                                        variables
             );
         }
-        notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(notificationTask, PROCESS_CASE_EVENT,
-                                   NOTIFY_RESPONDENT_SOLICITOR1_FOR_COURT_OFFICER_ORDER,
-                                   NOTIFY_RESPONDENT_SOLICITOR1_FOR_COURT_OFFICER_ORDER_ACTIVITY_ID,
-                                   variables
-        );
-
-        //end business process
-        ExternalTask endBusinessProcess = assertNextExternalTask(END_BUSINESS_PROCESS);
-        completeBusinessProcess(endBusinessProcess);
-
-        assertNoExternalTasksLeft();
-    }
-    @Test
-    void shouldSuccessfullyCompleteGenerateOrderNotificationsLipAndGenerateDashboard() {
-        //assert process has started
-        assertFalse(processInstance.isEnded());
-
-        //assert message start event
-        assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
-
-        VariableMap variables = Variables.createVariables();
-        variables.put("flowFlags", Map.of(
-            UNREPRESENTED_DEFENDANT_TWO, false,
-            DASHBOARD_SERVICE_ENABLED, true,
-            CASE_PROGRESSION_ENABLED, true));
-
-        //complete the start business process
-        ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
-        assertCompleteExternalTask(startBusiness, START_BUSINESS_TOPIC,
-                                   START_BUSINESS_EVENT, START_BUSINESS_ACTIVITY, variables);
-
-        ExternalTask notificationTask;
-
-        //complete the claimant notification
-        notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(notificationTask, PROCESS_CASE_EVENT,
-                                   NOTIFY_APPLICANT_SOLICITOR1_FOR_COURT_OFFICER_ORDER,
-                                   NOTIFY_APPLICANT_SOLICITOR1_FOR_COURT_OFFICER_ORDER_ACTIVITY_ID,
-                                   variables
-        );
         notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
         assertCompleteExternalTask(notificationTask, PROCESS_CASE_EVENT,
                                    NOTIFY_RESPONDENT_SOLICITOR1_FOR_COURT_OFFICER_ORDER,
