60a39c88fe058fb435e2558e84cf6b28d97d7f55
meulendijksm <max.meulendijks@hmcts.net>
2024-12-18 16:54:38 +0000

    CIV-15723 - Trial Ready Check test checkstyle fix

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/TrialReadyCheckTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/TrialReadyCheckTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TrialReadyCheckTest extends BpmnBaseTest {
-    
+
     private static final String MESSAGE_NAME = "TRIAL_READY_CHECK";
     private static final String PROCESS_ID = "TRIAL_READY_CHECK_PROCESS_ID";
     private static final String CLAIMANT_DASHBOARD_CHANGES = "CREATE_DASHBOARD_NOTIFICATION_TRIAL_READY_CHECK_CLAIMANT1";
@@ -16,6 +16,7 @@ public class TrialReadyCheckTest extends BpmnBaseTest {
     private static final String DEFENDANT_DASHBOARD_CHANGES_ID = "TrialReadyCheckDashboardNotificationsForDefendant1";
 
     public TrialReadyCheckTest() {
+        
         super("trial_ready_check.bpmn", "TRIAL_READY_CHECK_PROCESS_ID");
     }
 
