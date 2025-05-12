2fbeb27ef27ab7f77aa6d7ecbf2e5f11717739d7
paul-pearson <paul.pearson@version1.com>
2025-05-12 15:49:21 +0100

    CIV-16865 Pause notifications for translation of settlement agreement

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/UploadTranslatedDocumentSettlementAgreementTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/UploadTranslatedDocumentSettlementAgreementTest.java
@@ -4,16 +4,15 @@ import org.camunda.bpm.engine.externaltask.ExternalTask;
 import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.Variables;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.Map;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
-class DefendantSignSettlementAgreementTest extends BpmnBaseTest {
+class UploadTranslatedDocumentSettlementAgreementTest extends BpmnBaseTest {
 
-    private static final String FILE_NAME = "defendant_sign_settlement_agreement.bpmn";
-    private static final String MESSAGE_NAME = "DEFENDANT_SIGN_SETTLEMENT_AGREEMENT";
-    private static final String PROCESS_ID = "DEFENDANT_SIGN_SETTLEMENT_AGREEMENT_PROCESS_ID";
+    public static final String MESSAGE_NAME = "UPLOAD_TRANSLATED_DOCUMENT_SETTLEMENT_AGREEMENT";
+    public static final String PROCESS_ID = "UPLOAD_TRANSLATED_SETTLEMENT_AGREEMENT_PROCESS_ID";
+
     private static final String NOTIFY_LIP_APPLICANT_FOR_SIGN_SETTLEMENT_AGREEMENT =
         "NOTIFY_LIP_APPLICANT_FOR_SIGN_SETTLEMENT_AGREEMENT";
     private static final String NOTIFY_LIP_RESPONDENT_FOR_SIGN_SETTLEMENT_AGREEMENT =
@@ -27,52 +26,37 @@ class DefendantSignSettlementAgreementTest extends BpmnBaseTest {
     private static final String NOTIFY_LIP_RESPONDENT_FOR_SIGN_SETTLEMENT_AGREEMENT_ID =
         "NotifyRespondentForSignSettlementAgreement";
 
-    private static final String GENERATE_LIP_SIGN_SETTLEMENT_AGREEMENT_FORM =
-            "GENERATE_LIP_SIGN_SETTLEMENT_AGREEMENT_FORM";
-    private static final String GENERATE_LIP_SIGN_SETTLEMENT_AGREEMENT_FORM_ID =
-            "GenerateSignSettlementAgreement";
 
-    public DefendantSignSettlementAgreementTest() {
-        super(FILE_NAME, PROCESS_ID);
+    public UploadTranslatedDocumentSettlementAgreementTest() {
+        super("upload_translated_document_settlement_agreement.bpmn", PROCESS_ID);
     }
 
     @Test
-    void shouldSuccessfullyProcessDefendantSignAgreement() {
-        assertProcessStartedWithMessage(MESSAGE_NAME, PROCESS_ID);
+    void shouldSuccessfullyCompleteUploadTranslatedSettlementAgreement() {
+        //assert process has started
+        assertFalse(processInstance.isEnded());
+
+        //assert message start event
+        assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
+
         VariableMap variables = Variables.createVariables();
-        variables.putValue(FLOW_FLAGS, Map.of(
-            WELSH_ENABLED, true
-        ));
-        startBusinessProcess(variables);
-        generateSettlementAgreementDoc();
+        //complete the start business process
+        ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
+        assertCompleteExternalTask(startBusiness,
+                                   START_BUSINESS_TOPIC,
+                                   START_BUSINESS_EVENT,
+                                   START_BUSINESS_ACTIVITY,
+                                   variables);
+
         notifyApplicantSignSettlementAgreement();
         notifyRespondentSignSettlementAgreement();
         generateDashboardNotificationSignSettlementAgreement();
-        endBusinessProcess();
-        assertNoExternalTasksLeft();
-    }
 
-    @ParameterizedTest
-    @CsvSource({"true,false,false", "false,true,false", "false,false,true"})
-    void shouldSkipTasksWhenLanguagePreferenceWelsh(boolean claimantBilingual, boolean defendantBilingual,
-                                                    boolean docsBilingual) {
-        assertProcessStartedWithMessage(MESSAGE_NAME, PROCESS_ID);
-        VariableMap variables = Variables.createVariables();
-        variables.putValue(FLOW_FLAGS, Map.of(
-            WELSH_ENABLED, true,
-            CLAIM_ISSUE_BILINGUAL, claimantBilingual,
-            RESPONDENT_RESPONSE_LANGUAGE_IS_BILINGUAL, defendantBilingual,
-            BILINGUAL_DOCS, docsBilingual
-        ));
-        startBusinessProcess(variables);
-        generateSettlementAgreementDoc();
-        endBusinessProcess();
-        assertNoExternalTasksLeft();
-    }
-
-    private void endBusinessProcess() {
+        //end business process
         ExternalTask endBusinessProcess = assertNextExternalTask(END_BUSINESS_PROCESS);
         completeBusinessProcess(endBusinessProcess);
+
+        assertNoExternalTasksLeft();
     }
 
     private void notifyApplicantSignSettlementAgreement() {
@@ -102,16 +86,6 @@ class DefendantSignSettlementAgreementTest extends BpmnBaseTest {
             PROCESS_CASE_EVENT,
             DASHBOARD_NOTIFICATION_FOR_SIGN_SETTLEMENT_AGREEMENT_EVENT_ID,
             DASHBOARD_NOTIFICATION_FOR_SIGN_SETTLEMENT_AGREEMENT_ACTIVITY_ID
-        );
-    }
-
-    private void generateSettlementAgreementDoc() {
-        ExternalTask notificationTask = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-                notificationTask,
-                PROCESS_CASE_EVENT,
-                GENERATE_LIP_SIGN_SETTLEMENT_AGREEMENT_FORM,
-                GENERATE_LIP_SIGN_SETTLEMENT_AGREEMENT_FORM_ID
         );
     }
 }
