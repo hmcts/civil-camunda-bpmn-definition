f5cfc8ed5eb8d75ae1e2c4f42992f08cd2273b6d
Shaun Francis <shaun.francis@hmcts.net>
2026-01-21 11:04:19 +0000

    Update create_sdo.bpmn for combined dashboard notifications

--- src/test/java/uk/gov/hmcts/reform/civil/bpmn/CreateSDOTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/bpmn/CreateSDOTest.java
@@ -33,9 +33,7 @@ class CreateSDOTest extends BpmnBaseTest {
         assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
 
         VariableMap variables = Variables.createVariables();
-        variables.putValue(FLOW_FLAGS, Map.of(
-            DASHBOARD_SERVICE_ENABLED, false
-        ));
+        variables.putValue(FLOW_FLAGS, Map.of());
 
         //complete the start business process
         ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
@@ -55,23 +53,13 @@ class CreateSDOTest extends BpmnBaseTest {
             variables
         );
 
-        //complete the notification to respondent 1 dashboard
-        ExternalTask respondent1DashboardNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-                respondent1DashboardNotification,
-                PROCESS_CASE_EVENT,
-                "CREATE_DASHBOARD_NOTIFICATION_UPLOAD_HEARING_DOCUMENTS_CLAIMANT",
-                "Activity_Notice_Hearing_Claimant",
-                variables
-        );
-        //complete the notification to defendant 1 dashboard
-        ExternalTask applicant1DashboardNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-                applicant1DashboardNotification,
-                PROCESS_CASE_EVENT,
-                "CREATE_DASHBOARD_NOTIFICATION_UPLOAD_HEARING_DOCUMENTS_DEFENDANT",
-                "Activity_Notice_Hearing_Defendant",
-                variables
+        //complete the Dashboard notifications for create SDO
+        ExternalTask dashboardTask = assertNextExternalTask(PROCESS_CASE_EVENT);
+        assertCompleteExternalTask(dashboardTask,
+                                   PROCESS_CASE_EVENT,
+                                   DASHBOARD_NOTIFICATION_EVENT,
+                                   GENERATE_DASHBOARD_NOTIFICATION_CREATE_SDO,
+                                   variables
         );
 
         //complete the Trigger and Update GA Location event
@@ -102,7 +90,6 @@ class CreateSDOTest extends BpmnBaseTest {
         VariableMap variables = Variables.createVariables();
         variables.putValue(FLOW_FLAGS, Map.of(
             UNREPRESENTED_DEFENDANT_ONE, true,
-            DASHBOARD_SERVICE_ENABLED, true,
             LIP_CASE, true
         ));
 
@@ -143,32 +130,13 @@ class CreateSDOTest extends BpmnBaseTest {
             variables
         );
 
-        //complete the Dashboard creation
+        //complete the Dashboard notifications for create SDO
         ExternalTask dashboardTask = assertNextExternalTask(PROCESS_CASE_EVENT);
         assertCompleteExternalTask(dashboardTask,
                                    PROCESS_CASE_EVENT,
                                    DASHBOARD_NOTIFICATION_EVENT,
                                    GENERATE_DASHBOARD_NOTIFICATION_CREATE_SDO,
                                    variables
-        );
-
-        //complete the notification to respondent 1 dashboard
-        ExternalTask respondent1DashboardNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            respondent1DashboardNotification,
-            PROCESS_CASE_EVENT,
-            "CREATE_DASHBOARD_NOTIFICATION_UPLOAD_HEARING_DOCUMENTS_CLAIMANT",
-            "Activity_Notice_Hearing_Claimant",
-            variables
-        );
-        //complete the notification to defendant 1 dashboard
-        ExternalTask applicant1DashboardNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            applicant1DashboardNotification,
-            PROCESS_CASE_EVENT,
-            "CREATE_DASHBOARD_NOTIFICATION_UPLOAD_HEARING_DOCUMENTS_DEFENDANT",
-            "Activity_Notice_Hearing_Defendant",
-            variables
         );
 
         //complete the Trigger and Update GA Location event
@@ -199,7 +167,6 @@ class CreateSDOTest extends BpmnBaseTest {
         VariableMap variables = Variables.createVariables();
         variables.putValue(FLOW_FLAGS, Map.of(
             UNREPRESENTED_DEFENDANT_ONE, false,
-            DASHBOARD_SERVICE_ENABLED, true,
             LIP_CASE, true
         ));
 
@@ -231,32 +198,13 @@ class CreateSDOTest extends BpmnBaseTest {
             variables
         );
 
-        //complete the Dashboard creation
+        //complete the Dashboard notifications for create SDO
         ExternalTask dashboardTask = assertNextExternalTask(PROCESS_CASE_EVENT);
         assertCompleteExternalTask(dashboardTask,
                                    PROCESS_CASE_EVENT,
                                    DASHBOARD_NOTIFICATION_EVENT,
                                    GENERATE_DASHBOARD_NOTIFICATION_CREATE_SDO,
                                    variables
-        );
-
-        //complete the notification to respondent 1 dashboard
-        ExternalTask respondent1DashboardNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            respondent1DashboardNotification,
-            PROCESS_CASE_EVENT,
-            "CREATE_DASHBOARD_NOTIFICATION_UPLOAD_HEARING_DOCUMENTS_CLAIMANT",
-            "Activity_Notice_Hearing_Claimant",
-            variables
-        );
-        //complete the notification to defendant 1 dashboard
-        ExternalTask applicant1DashboardNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            applicant1DashboardNotification,
-            PROCESS_CASE_EVENT,
-            "CREATE_DASHBOARD_NOTIFICATION_UPLOAD_HEARING_DOCUMENTS_DEFENDANT",
-            "Activity_Notice_Hearing_Defendant",
-            variables
         );
 
         //complete the Trigger and Update GA Location event
@@ -287,7 +235,6 @@ class CreateSDOTest extends BpmnBaseTest {
         VariableMap variables = Variables.createVariables();
         variables.putValue(FLOW_FLAGS, Map.of(
             UNREPRESENTED_DEFENDANT_ONE, false,
-            DASHBOARD_SERVICE_ENABLED, true,
             LIP_CASE, false
         ));
 
@@ -309,7 +256,7 @@ class CreateSDOTest extends BpmnBaseTest {
             variables
         );
 
-        //complete the Dashboard creation
+        //complete the Dashboard notifications for create SDO
         ExternalTask dashboardTask = assertNextExternalTask(PROCESS_CASE_EVENT);
         assertCompleteExternalTask(dashboardTask,
                                    PROCESS_CASE_EVENT,
@@ -318,112 +265,6 @@ class CreateSDOTest extends BpmnBaseTest {
                                    variables
         );
 
-        //complete the notification to respondent 1 dashboard
-        ExternalTask respondent1DashboardNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            respondent1DashboardNotification,
-            PROCESS_CASE_EVENT,
-            "CREATE_DASHBOARD_NOTIFICATION_UPLOAD_HEARING_DOCUMENTS_CLAIMANT",
-            "Activity_Notice_Hearing_Claimant",
-            variables
-        );
-        //complete the notification to defendant 1 dashboard
-        ExternalTask applicant1DashboardNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            applicant1DashboardNotification,
-            PROCESS_CASE_EVENT,
-            "CREATE_DASHBOARD_NOTIFICATION_UPLOAD_HEARING_DOCUMENTS_DEFENDANT",
-            "Activity_Notice_Hearing_Defendant",
-            variables
-        );
-
-        //complete the Trigger and Update GA Location event
-        ExternalTask triggerAndUpdateGenAppLocation = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            triggerAndUpdateGenAppLocation,
-            PROCESS_CASE_EVENT,
-            TRIGGER_UPDATE_GA_LOCATION,
-            TRIGGER_UPDATE_GA_LOCATION_ACTIVITY_ID,
-            variables
-        );
-
-        //end business process
-        ExternalTask endBusinessProcess = assertNextExternalTask(END_BUSINESS_PROCESS);
-        completeBusinessProcess(endBusinessProcess);
-
-        assertNoExternalTasksLeft();
-    }
-
-    @Test
-    void shouldSuccessfullyPassDashboardTasksWhenDashboardServiceEnabled() {
-        //assert process has started
-        assertFalse(processInstance.isEnded());
-
-        //assert message start event
-        assertThat(getProcessDefinitionByMessage(MESSAGE_NAME).getKey()).isEqualTo(PROCESS_ID);
-
-        VariableMap variables = Variables.createVariables();
-        variables.putValue(FLOW_FLAGS, Map.of(
-            UNREPRESENTED_DEFENDANT_ONE, true,
-            DASHBOARD_SERVICE_ENABLED, true,
-            CLAIM_ISSUE_BILINGUAL, true
-        ));
-
-        //complete the start business process
-        ExternalTask startBusiness = assertNextExternalTask(START_BUSINESS_TOPIC);
-        assertCompleteExternalTask(startBusiness,
-                                   START_BUSINESS_TOPIC,
-                                   START_BUSINESS_EVENT,
-                                   START_BUSINESS_ACTIVITY,
-                                   variables);
-
-        //complete the notification to parties
-        ExternalTask partiesNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            partiesNotification,
-            PROCESS_CASE_EVENT,
-            "NOTIFY_EVENT",
-            "CreateSDONotify",
-            variables
-        );
-
-        //Trigger Bulk Print
-        ExternalTask sendSDOOrder = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            sendSDOOrder,
-            PROCESS_CASE_EVENT,
-            "SEND_SDO_ORDER_TO_LIP_DEFENDANT",
-            "SendSDOToDefendantLIP",
-            variables
-        );
-
-        //complete the Dashboard creation
-        ExternalTask dashboardTask = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(dashboardTask,
-                                   PROCESS_CASE_EVENT,
-                                   DASHBOARD_NOTIFICATION_EVENT,
-                                   GENERATE_DASHBOARD_NOTIFICATION_CREATE_SDO,
-                                   variables
-        );
-
-        //complete the notification to respondent 1 dashboard
-        ExternalTask respondent1DashboardNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            respondent1DashboardNotification,
-            PROCESS_CASE_EVENT,
-            "CREATE_DASHBOARD_NOTIFICATION_UPLOAD_HEARING_DOCUMENTS_CLAIMANT",
-            "Activity_Notice_Hearing_Claimant",
-            variables
-        );
-        //complete the notification to defendant 1 dashboard
-        ExternalTask applicant1DashboardNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            applicant1DashboardNotification,
-            PROCESS_CASE_EVENT,
-            "CREATE_DASHBOARD_NOTIFICATION_UPLOAD_HEARING_DOCUMENTS_DEFENDANT",
-            "Activity_Notice_Hearing_Defendant",
-            variables
-        );
 
         //complete the Trigger and Update GA Location event
         ExternalTask triggerAndUpdateGenAppLocation = assertNextExternalTask(PROCESS_CASE_EVENT);
@@ -454,7 +295,6 @@ class CreateSDOTest extends BpmnBaseTest {
         VariableMap variables = Variables.createVariables();
         variables.putValue(FLOW_FLAGS, Map.of(
             WELSH_ENABLED, true,
-            DASHBOARD_SERVICE_ENABLED, true,
             CLAIM_ISSUE_BILINGUAL, claimantBilingual,
             RESPONDENT_RESPONSE_LANGUAGE_IS_BILINGUAL, defendantBilingual
         ));
@@ -467,32 +307,13 @@ class CreateSDOTest extends BpmnBaseTest {
                                    START_BUSINESS_ACTIVITY,
                                    variables);
 
-        //complete the Dashboard creation
+        //complete the Dashboard notifications for create SDO
         ExternalTask dashboardTask = assertNextExternalTask(PROCESS_CASE_EVENT);
         assertCompleteExternalTask(dashboardTask,
                                    PROCESS_CASE_EVENT,
                                    DASHBOARD_NOTIFICATION_EVENT,
                                    GENERATE_DASHBOARD_NOTIFICATION_CREATE_SDO,
                                    variables
-        );
-
-        //complete the notification to respondent 1 dashboard
-        ExternalTask respondent1DashboardNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            respondent1DashboardNotification,
-            PROCESS_CASE_EVENT,
-            "CREATE_DASHBOARD_NOTIFICATION_UPLOAD_HEARING_DOCUMENTS_CLAIMANT",
-            "Activity_Notice_Hearing_Claimant",
-            variables
-        );
-        //complete the notification to defendant 1 dashboard
-        ExternalTask applicant1DashboardNotification = assertNextExternalTask(PROCESS_CASE_EVENT);
-        assertCompleteExternalTask(
-            applicant1DashboardNotification,
-            PROCESS_CASE_EVENT,
-            "CREATE_DASHBOARD_NOTIFICATION_UPLOAD_HEARING_DOCUMENTS_DEFENDANT",
-            "Activity_Notice_Hearing_Defendant",
-            variables
         );
 
         //complete the Trigger and Update GA Location event
